7add534d04ff6fe4e78bf6f9b9e0460838e8ffb9
Riabkov, Viktor <vriabkov@scnsoft.com>
2022-11-16 19:34:29 +0200

    feature: architecture of the project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
+import reactLogo from "../assets/react.svg"
 import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
-
-  console.log(count)
 
   return (
     <div className="App">
