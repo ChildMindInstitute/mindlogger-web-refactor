5f68f1ff3f1fcbd254aa8f50ebeae5e787254dd6
Riabkov, Viktor <vriabkov@scnsoft.com>
2022-11-16 19:05:03 +0200

    feature: initial prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  console.log(count)
 
   return (
     <div className="App">
