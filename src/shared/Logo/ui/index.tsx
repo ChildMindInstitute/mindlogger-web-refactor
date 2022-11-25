610abd1e20d7b8bf72e4211f93a1101128232028
Riabkov, Viktor <vriabkov@scnsoft.com>
2022-11-25 16:40:52 +0200

    feature: footer mobile response

--- src/shared/Logo/ui/index.tsx
+++ src/shared/Logo/ui/index.tsx
@@ -3,19 +3,22 @@ import { LOGO_URL } from "../lib/constants"
 export interface LogoProps {
   widthPx?: number
   heightPx?: number
+  classNameExtended?: string
 }
 
-const Logo = ({ widthPx, heightPx }: LogoProps) => {
+const Logo = ({ widthPx, heightPx, classNameExtended }: LogoProps) => {
   const defaultSize = {
     height: 50,
     width: 120,
   }
 
+  const defaultClassName = "mr-1 p-1"
+
   return (
     <img
       height={heightPx || defaultSize.height}
       width={widthPx || defaultSize.width}
-      className="logo mr-1 xs-display-none p-1"
+      className={classNameExtended ? `${defaultClassName} ${classNameExtended}` : defaultClassName}
       src={LOGO_URL}
       loading="lazy"
     />
--- src/widgets/Footer/ui/index.tsx
+++ src/widgets/Footer/ui/index.tsx
@@ -2,14 +2,14 @@ import Logo from "~/shared/Logo"
 
 const Footer = () => {
   return (
-    <footer className="d-flex justify-content-center align-items-center bg-light footer">
+    <footer className="d-flex justify-content-center align-items-center bg-light">
       <div className="mt-3 mb-3 text-center d-flex align-items-center">
-        <Logo />
-        <span className="xs-display-none">&#169;</span>
+        <Logo classNameExtended="d-none d-sm-block" />
+        <span className="d-none d-sm-block">&#169;</span>
         <a href="https://childmind.org" className="mx-1" target="_blank" rel="noreferrer">
           Child Mind Institute
         </a>
-        <span className="xs-display-none">MATTER Lab 2022</span>
+        <span className="d-none d-sm-block">MATTER Lab 2022</span>
         <a className="mx-4" href="https://mindlogger.org/terms" target="_blank" rel="noreferrer">
           Terms of Service
         </a>
