5486696d7a127915927c7f6eb7eb5a37c38a62b4
Riabkov, Viktor <vriabkov@scnsoft.com>
2022-11-25 17:19:56 +0200

    feature: inital structure of content layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,16 @@
 import { ReactNode } from "react"
-import { useLocation } from "react-router-dom"
 
-import Header from "../widgets/Header"
+import Header from "../widgets/header"
 
 interface LayoutProps {
   children: ReactNode
 }
 
 const Layout = (props: LayoutProps): null | JSX.Element => {
-  const location = useLocation() // TODO: remove it - just for initial steps of development
-
   return (
     <>
       <Header />
-      <div>{props.children}</div>
-      <div>Some footer: location - {location.pathname}</div>
+      <div className="content-container">{props.children}</div>
     </>
   )
 }
