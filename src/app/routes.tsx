b1347013627c5c253f2678fa5afaa535a0c3871d
Riabkov, Viktor <vriabkov@scnsoft.com>
2022-11-28 18:12:11 +0200

    feature: react-hook-form and yup

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom"
+import { LoginPage } from "../pages"
 import { ROUTES } from "./system/routes/constants"
 
 const ApplicationRouter = (): JSX.Element | null => {
   return (
     <Routes>
       <Route path={ROUTES.main.path} />
-      <Route path={ROUTES.login.path} />
+      <Route path={ROUTES.login.path} element={<LoginPage />} />
       <Route path={ROUTES.signup.path} />
     </Routes>
   )
