02529a30cb3502391ca19037eae45682c22db21c
Riabkov, Viktor <vriabkov@scnsoft.com>
2022-11-29 10:35:36 +0200

    fix: case sensitive file names

---
+++ src/app/Layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react"
+
+import Footer from "../widgets/Footer"
+import Header from "../widgets/Header"
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+const Layout = (props: LayoutProps): null | JSX.Element => {
+  return (
+    <>
+      <Header />
+      <div className="content-container">
+        <div className="content">{props.children}</div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
---
+++ src/widgets/Header/lib/useNavbarTranslation.ts
@@ -0,0 +1,8 @@
+import { useCustomTranslation } from "~/utils/hooks/useCustomTranslation"
+import { UseTranslationOutput } from "../../../utils/types/useTranslationOutput"
+
+export const useNavbarTranslation = (): UseTranslationOutput => {
+  const { t, i18n } = useCustomTranslation({ keyPrefix: "Navbar" })
+
+  return { t, i18n }
+}
