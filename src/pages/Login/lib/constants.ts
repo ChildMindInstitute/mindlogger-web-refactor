69c475020990efeede9cb25112b3de2d295ca31b
Riabkov, Viktor <vriabkov@scnsoft.com>
2022-12-05 13:24:03 +0200

    feature: login constants

---
+++ src/pages/Login/lib/constants.ts
@@ -0,0 +1,3 @@
+export const APPSTORE_LINK = "https://apps.apple.com/us/app/mindlogger-pilot/id1301092229#?platform=iphone"
+export const GOOGLEPLAY_LINK =
+  "https://play.google.com/store/apps/details?id=com.childmindinstitute.exposuretherapy&hl=en_US&gl=US"
