829085cb5c3fafd0d6ae65cb450a9b98f58c2556
Riabkov, Viktor <vriabkov@scnsoft.com>
2022-12-30 15:15:05 +0400

    fix: types

---
+++ src/declaration.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="redux-persist" />
+
+declare module "redux-persist/integration/react" {
+  import { ReactNode, PureComponent } from "react"
+  import { Persistor } from "redux-persist/es/types"
+
+  /** @see PersistGate */
+  interface PersistGateProps {
+    persistor: Persistor
+    onBeforeLift?(): void | Promise<void>
+    children?: ReactNode | ((bootstrapped: boolean) => ReactNode)
+    loading?: ReactNode
+  }
+
+  /** @see PersistGate */
+  interface PersistorGateState {
+    bootstrapped: boolean
+  }
+
+  /**
+   * Entry point of your react application to allow it persist a given store @see Persistor and its configuration.
+   * @see Persistor
+   * @see PersistGateProps
+   * @see PersistGateState
+   */
+  class PersistGate extends React.PureComponent<PersistGateProps, PersistorGateState> {}
+}
--- src/react-i18next.d.ts
+++
@@ -1,9 +0,0 @@
-import "react-i18next"
-import en from "./i18n/en/translation.json"
-
-declare module "react-i18next" {
-  interface CustomTypeOptions {
-    defaultNS: "translation"
-    resources: typeof en
-  }
-}
