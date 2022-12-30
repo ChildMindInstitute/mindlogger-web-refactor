6f911152fc4a91796ac0a1da7cb8b89ef26b1de1
Victor Ryabkov <45964820+moiskillnadne@users.noreply.github.com>
2022-12-30 15:04:39 +0300

    Delete src/widgets/header directory

--- src/widgets/header/lib/useNavbarTranslation.ts
+++
@@ -1,7 +0,0 @@
-import { UseTranslationOutput, useCustomTranslation } from "~/shared"
-
-export const useNavbarTranslation = (): UseTranslationOutput => {
-  const { t, i18n } = useCustomTranslation({ keyPrefix: "Navbar" })
-
-  return { t, i18n }
-}
