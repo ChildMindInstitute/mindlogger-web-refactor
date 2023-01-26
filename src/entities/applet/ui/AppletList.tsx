5064693d1c9e05e0b55b6653b564b7d0c5c20a07
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-01-26 15:26:21 +0400

    feature: spinner while applet list fetching

--- src/entities/applet/ui/AppletList.tsx
+++ src/entities/applet/ui/AppletList.tsx
@@ -1,16 +1,20 @@
-import { Row } from "react-bootstrap"
+import classNames from "classnames"
+import { Container, Row, Spinner } from "react-bootstrap"
 
 import { useAppletListQuery } from "../api"
 import AppletCard from "./AppletCard"
 
 const AppletList = () => {
-  const { data } = useAppletListQuery()
+  const { data, isLoading } = useAppletListQuery()
 
   return (
-    <Row className="applet-list justify-content-center">
-      {data?.data.results.map(value => (
-        <AppletCard key={value.id} applet={value} />
-      ))}
+    <Row className={classNames("applet-list", "justify-content-center", { "h-100": isLoading })}>
+      {isLoading && (
+        <Container className={classNames("d-flex", "h-100", "w-100", "justify-content-center", "align-items-center")}>
+          <Spinner as="div" animation="border" role="status" aria-hidden="true" />
+        </Container>
+      )}
+      {!isLoading && data?.data.results.map(value => <AppletCard key={value.id} applet={value} />)}
     </Row>
   )
 }
