05b35e07e29c39de8ff21b9c786f499e898318a2
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-01-31 12:27:25 +0400

    fix: change config

--- vite.config.ts
+++ vite.config.ts
@@ -20,31 +20,6 @@ export default defineConfig(({ command, mode }) => {
           '~': resolve(__dirname, 'src'),
         },
       },
-      server: {
-        proxy: {
-          "/api": {
-               target: 'https://api-dev.cmiml.net/',
-               changeOrigin: true,
-               secure: false,
-               rewrite: (path) => path.replace(/^\/api/, ''),
-               ws: true,
-               configure: (proxy, options) => {
-                console.log(proxy)
-                console.log(options)
-  
-                proxy.on('error', (err, _req, _res) => {
-                  console.log('proxy error', err);
-                });
-                proxy.on('proxyReq', (proxyReq, req, _res) => {
-                  console.log('Sending Request to the Target:', req.method, req.url);
-                });
-                proxy.on('proxyRes', (proxyRes, req, _res) => {
-                  console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-                });
-              },
-          },
-        },
-      }
     }
   } else if(command === 'build') {
     return {
@@ -59,31 +34,6 @@ export default defineConfig(({ command, mode }) => {
           '~': resolve(__dirname, 'src'),
         },
       },
-      server: {
-        proxy: {
-          "/api": {
-               target: 'https://api-dev.cmiml.net/',
-               changeOrigin: true,
-               secure: false,
-               rewrite: (path) => path.replace(/^\/api/, ''),
-               ws: true,
-               configure: (proxy, options) => {
-                console.log(proxy)
-                console.log(options)
-  
-                proxy.on('error', (err, _req, _res) => {
-                  console.log('proxy error', err);
-                });
-                proxy.on('proxyReq', (proxyReq, req, _res) => {
-                  console.log('Sending Request to the Target:', req.method, req.url);
-                });
-                proxy.on('proxyRes', (proxyRes, req, _res) => {
-                  console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-                });
-              },
-          },
-        },
-      }
     }
   }
 })
