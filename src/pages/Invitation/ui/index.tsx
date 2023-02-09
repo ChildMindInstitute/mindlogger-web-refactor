517d62ac4f215903970d0b7aadc09d72fb82e4fe
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-02-09 15:17:03 +0400

    feature: Invitation page

--- src/pages/Invitation/ui/index.tsx
+++ src/pages/Invitation/ui/index.tsx
@@ -1,9 +1,23 @@
+import classNames from "classnames"
+import { Container } from "react-bootstrap"
 import { useSearchParams } from "react-router-dom"
+
+import { InvitationWidget } from "~/widgets/Invitation"
 
 const InvitationPage = () => {
   const [searchParams] = useSearchParams()
 
-  return <div>{searchParams.get("key")}</div>
+  const keyParams = searchParams.get("key")
+
+  if (!keyParams) {
+    return <div>Key not exist error TODO: add correct error text</div>
+  }
+
+  return (
+    <Container className={classNames("mt-3", "pt-3")}>
+      <InvitationWidget keyParams={keyParams} />
+    </Container>
+  )
 }
 
 export default InvitationPage
