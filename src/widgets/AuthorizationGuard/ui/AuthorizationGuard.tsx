60d45dee24c5eebfba8ea8458f2baf7dcb7d6f08
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-02-16 20:32:42 +0400

    feature: useAuthorizationGuard hook

--- src/widgets/AuthorizationGuard/ui/AuthorizationGuard.tsx
+++ src/widgets/AuthorizationGuard/ui/AuthorizationGuard.tsx
@@ -1,16 +1,13 @@
 import { PropsWithChildren, ReactNode } from "react"
 
-import { userModel } from "~/entities/user"
+import { useAuthorizationGuard } from "../lib"
 
 interface AuthorizationGuardProps extends PropsWithChildren {
   fallback: ReactNode
 }
 
 export const AuthorizationGuard = ({ fallback, children }: AuthorizationGuardProps) => {
-  const { user } = userModel.hooks.useUserState()
-  const tokens = userModel.hooks.useTokensState()
-
-  const isAuthenticated = user.id && tokens?.accessToken
+  const { isAuthenticated } = useAuthorizationGuard()
 
   if (!isAuthenticated) {
     return <>{fallback}</>
