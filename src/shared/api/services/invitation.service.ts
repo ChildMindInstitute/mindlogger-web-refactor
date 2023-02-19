57ce4b16608173cea9fd5147ed08d9bb974caef2
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-02-20 00:23:35 +0400

    changes: Change endpoint url after backend update

--- src/shared/api/services/invitation.service.ts
+++ src/shared/api/services/invitation.service.ts
@@ -12,10 +12,10 @@ function invitationService() {
       return axiosService.get<GetInvitationSuccessResponse>(`/invitations/${payload.invitationId}`)
     },
     acceptInvitation(payload: AcceptInvitationByIdPayload) {
-      return axiosService.post(`/invitations/approve/${payload.invitationId}`)
+      return axiosService.post(`/invitations/${payload.invitationId}/accept`)
     },
     declineInvitation(payload: DeclineInvitationByIdPayload) {
-      return axiosService.post(`/invitations/decline/${payload.invitationId}`)
+      return axiosService.post(`/invitations/${payload.invitationId}/decline`)
     },
 
     getPrivateInvitationById(payload: GetInvitationByIdPayload) {
