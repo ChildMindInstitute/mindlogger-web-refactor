e581bb0b87ecb33d8960fd8eeafedfd5ba6b111c
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-02-24 10:56:22 +0400

    feature: ActivityProgressPreviewList component

--- src/widgets/ActivityDetails/ui/ActivityDetails.tsx
+++ src/widgets/ActivityDetails/ui/ActivityDetails.tsx
@@ -1,13 +1,12 @@
 import classNames from "classnames"
-import { Container, Row, Col, Card, ProgressBar } from "react-bootstrap"
-import { ArrowLeft } from "react-bootstrap-icons"
-import { useNavigate } from "react-router-dom"
+import { Container, Row, Col } from "react-bootstrap"
 
-import { AllActivityStatusMap } from "./AllActivityStatusMap"
+import { ActivityCardItemList } from "../../../entities/item/ui/ActivityCardItemList"
+import { useAppletByIdQuery } from "../../ActivityGroups"
+import { mockActivityList } from "../lib/activityList.mock"
 
-import { BasicButton } from "~/shared/ui"
+import { ActivityProgressPreviewList } from "~/entities/activity"
 import CustomCard from "~/shared/ui/Card"
-import { useCustomTranslation } from "~/shared/utils"
 
 interface ActivityDetailsWidgetProps {
   appletId: string
@@ -15,74 +14,35 @@ interface ActivityDetailsWidgetProps {
 }
 
 export const ActivityDetailsWidget = ({ appletId, activityId }: ActivityDetailsWidgetProps) => {
-  const { t } = useCustomTranslation()
-  const navigate = useNavigate()
+  const { data } = useAppletByIdQuery({ isPublic: false, appletId })
 
-  const isOnePageAssessment = false // Mock
-  const isSummaryScreen = true // Mock
-  const percentage = 35 // Mock
+  const appletDetails = data?.data?.result
 
-  const mockActivityStatus = [
-    { label: "activity 1", percentage: 20 },
-    { label: "activity 2", percentage: 0 },
-    { label: "activity 3", percentage: 70 },
-  ] // Mock
-
-  const onBackButtonClick = () => {
-    navigate(-1)
-  }
+  const isOnePageAssessment = true // Mock
+  const isSummaryScreen = false // Mock
 
   return (
     <Container>
-      <Row className="mt-5">
-        <Col className="" xl={3}>
-          <BasicButton variant="primary" className="mb-2 d-flex align-items-center" onClick={onBackButtonClick}>
-            <ArrowLeft className="me-1" />
-            {t("Consent.back")}
-          </BasicButton>
-        </Col>
-        {!isOnePageAssessment && (
-          <Col xl={9}>
-            <Card className="bg-white p-2">
-              <ProgressBar striped className="mb-2" now={percentage} variant="primary" />
-            </Card>
-          </Col>
-        )}
-      </Row>
       <Row className="mt-2 activity">
         <Col xl={3}>
           <div className={classNames("d-flex", "justify-content-center")}>
-            <CustomCard
-              id={"someid"}
-              type="card"
-              title={"title"}
-              imageSrc={"https://picsum.photos/200"}
-              className={classNames("hover", "mb-4", "activity-details-card")}
-            />
+            {appletDetails && (
+              <CustomCard
+                id={appletDetails.id}
+                type="card"
+                title={appletDetails.displayName}
+                imageSrc={appletDetails.image}
+                className={classNames("hover", "mb-4", "activity-details-card")}
+              />
+            )}
           </div>
 
-          {/* Ask BA what is it? */}
-          {/* {headers.map(itemHeader => (
-            <div className="mx-4">
-              {itemHeader.headerName && (
-                <div onClick={() => selectHeader(itemHeader.id)} className="mt-1 header-text">
-                  {itemHeader.headerName}
-                </div>
-              )}
-              {itemHeader.sectionName && (
-                <div onClick={() => selectHeader(itemHeader.id)} className="ml-4 section-text">
-                  {` - ${itemHeader.sectionName}`}
-                </div>
-              )}
-            </div>
-          ))} */}
-
-          <AllActivityStatusMap activityStatus={mockActivityStatus} />
+          <ActivityProgressPreviewList activities={mockActivityList} />
         </Col>
         <Col xl={9}>
           {/* Should be implemented after ITEMs */}
           {/* {isSummaryScreen && <ActivitySummary />} */}
-          {/* {!isSummaryScreen && isOnePageAssessment && items} */}
+          {!isSummaryScreen && isOnePageAssessment && <ActivityCardItemList />}
           {/* {!isSummaryScreen && !isOnePageAssessment && _.map(items.slice(0, availableItems).reverse())} */}
         </Col>
       </Row>
