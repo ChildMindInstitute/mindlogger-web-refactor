a7f6333e7ab97d57f8fcd9c92111127aa42726fe
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-02-24 12:43:22 +0400

    fix: params guard ActivityDetails and ActivityList pages

--- src/pages/ActivityDetails/ui/index.tsx
+++ src/pages/ActivityDetails/ui/index.tsx
@@ -1,14 +1,20 @@
 import { Container } from "react-bootstrap"
 import { useParams } from "react-router-dom"
 
+import { useCustomTranslation } from "~/shared/utils"
 import { ActivityDetailsWidget } from "~/widgets/ActivityDetails"
 
 export const ActivityDetailsPage = () => {
   const { appletId, activityId } = useParams()
+  const { t } = useCustomTranslation()
+
+  if (!appletId && !activityId) {
+    return <div>{t("wrondLinkParametrError")}</div>
+  }
 
   return (
     <Container>
-      {appletId && activityId && <ActivityDetailsWidget appletId={appletId} activityId={activityId} />}
+      <ActivityDetailsWidget appletId={appletId!} activityId={activityId!} />
     </Container>
   )
 }
