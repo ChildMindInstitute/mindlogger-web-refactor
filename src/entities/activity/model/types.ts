4b485ec49d94607c11a88b98832cecbe2a74f37e
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-03-07 15:01:30 +0400

    feature: redux store interface updated

--- src/entities/activity/model/types.ts
+++ src/entities/activity/model/types.ts
@@ -1,9 +1,26 @@
+import { ActivityItemType } from "../../item"
+
+export type ProgressPayloadAnswer = {
+  itemId: string
+  question: string
+  responseType: ActivityItemType
+
+  isSkippable: boolean
+  isRandom: boolean
+  isAbleToMoveToPrevious: boolean
+  hasTextResponse: boolean
+  ordering: number
+
+  answer: string | null
+}
+
 export type ProgressPayloadState = {
   appletId: string
   activityId: string
   eventId: string
   startAt: Date | null
   endAt: Date | null
+  answers: ProgressPayloadAnswer[]
 }
 
 export type ActivityProgressState = Array<ProgressPayloadState>
