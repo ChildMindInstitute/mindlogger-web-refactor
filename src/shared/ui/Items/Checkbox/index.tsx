480d1211448c4dedf5b8d07a98a74ee3c20609ee
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-03-26 06:38:10 +0400

    feature: CheckboxItem Component

---
+++ src/shared/ui/Items/Checkbox/index.tsx
@@ -0,0 +1,49 @@
+import { Form, Image } from "react-bootstrap"
+
+import { CustomTooltip } from "../../Tooltip"
+
+import "./style.scss"
+
+type CheckboxItemProps = {
+  key?: string
+
+  id: string
+  name: string
+  value: string
+  label: string
+
+  disabled?: boolean
+  image?: string
+  description?: string
+  color?: string
+  defaultChecked?: boolean
+
+  onChange: (value: string) => void
+}
+
+export const CheckboxItem = (props: CheckboxItemProps) => {
+  const { id, key, name, value, label, image, description, disabled, defaultChecked, onChange, color } = props
+
+  return (
+    <div key={key} className="response-option" style={{ background: color ? color : "none" }}>
+      <div className="option-tooltip">{description && <CustomTooltip markdown={description} />}</div>
+
+      {image && (
+        <div className="option-image">
+          <Image src={image} roundedCircle />
+        </div>
+      )}
+      <Form.Check
+        id={id}
+        type="checkbox"
+        name={name}
+        className="form-check-width"
+        value={value}
+        label={label}
+        disabled={disabled}
+        defaultChecked={defaultChecked}
+        onChange={e => onChange(e.target.value)}
+      />
+    </div>
+  )
+}
