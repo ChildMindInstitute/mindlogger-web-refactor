a43af0a101ab245ea13e22f4295d70acfb3e477b
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-03-26 06:17:43 +0400

    feature: Tooltip component

---
+++ src/shared/ui/Tooltip/index.tsx
@@ -0,0 +1,25 @@
+import { OverlayTrigger, Tooltip, Image } from "react-bootstrap"
+
+import { Markdown } from "../Markdown"
+
+import questionMark from "~/assets/question-mark.svg"
+import "./style.scss"
+
+type TooltipProps = {
+  markdown: string
+}
+
+export const CustomTooltip = ({ markdown }: TooltipProps) => {
+  return (
+    <OverlayTrigger
+      placement="bottom"
+      delay={{ show: 250, hide: 200 }}
+      overlay={
+        <Tooltip id="button-tooltip">
+          <Markdown markdown={markdown} />
+        </Tooltip>
+      }>
+      <Image src={questionMark} className="tooltip-icon" />
+    </OverlayTrigger>
+  )
+}
