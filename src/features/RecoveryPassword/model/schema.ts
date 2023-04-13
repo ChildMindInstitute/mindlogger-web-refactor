19ca27881da040b18c0734ff3b9c75ea2d14ace6
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-04-13 22:57:21 +0400

    fix: M2-673 password do not match validation recovery password form

--- src/features/RecoveryPassword/model/schema.ts
+++ src/features/RecoveryPassword/model/schema.ts
@@ -4,12 +4,12 @@ import { Dictionary } from "~/shared/utils"
 
 export const RecoveryPasswordSchema = z
   .object({
-    new: z.string().min(6, { message: Dictionary.validation.password.required }),
-    confirm: z.string().min(6, { message: Dictionary.validation.password.required }),
+    new: z.string().trim().min(6, { message: Dictionary.validation.password.minLength }),
+    confirm: z.string().trim().min(6, { message: Dictionary.validation.password.minLength }),
   })
   .refine(data => data.new === data.confirm, {
     message: Dictionary.validation.password.notMatch,
-    path: ["confirmNewPassword"],
+    path: ["confirm"],
   })
 
 export type RecoveryPassword = z.infer<typeof RecoveryPasswordSchema>
