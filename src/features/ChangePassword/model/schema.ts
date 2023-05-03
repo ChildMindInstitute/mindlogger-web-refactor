68dbd0a2bd3ddbe55a7baacbf53e27ba99eff340
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-05-03 23:53:13 +0400

    fix: password should not contain spaces settings

--- src/features/ChangePassword/model/schema.ts
+++ src/features/ChangePassword/model/schema.ts
@@ -2,11 +2,25 @@ import { z } from "zod"
 
 import { Dictionary } from "~/shared/utils"
 
+const isIncludesSpaces = (value: string) => value.includes(" ")
+
 export const ChangePasswordSchema = z
   .object({
     old: z.string().trim().min(6, { message: Dictionary.validation.password.minLength }),
-    new: z.string().trim().min(6, { message: Dictionary.validation.password.minLength }),
-    confirm: z.string().trim().min(6, { message: Dictionary.validation.password.minLength }),
+    new: z
+      .string()
+      .trim()
+      .min(6, { message: Dictionary.validation.password.minLength })
+      .refine(value => !isIncludesSpaces(value), {
+        message: Dictionary.validation.password.shouldNotContainSpaces,
+      }),
+    confirm: z
+      .string()
+      .trim()
+      .min(6, { message: Dictionary.validation.password.minLength })
+      .refine(value => !isIncludesSpaces(value), {
+        message: Dictionary.validation.password.shouldNotContainSpaces,
+      }),
   })
   .refine(data => data.new === data.confirm, {
     message: Dictionary.validation.password.notMatch,
