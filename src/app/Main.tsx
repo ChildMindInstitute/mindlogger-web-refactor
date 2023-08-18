bf61462286d06d09ff78bea1faaa99650b244caa
DimitrySakalouski <dsakalouski@scnsoft.com>
2023-08-18 17:45:13 +0300

    refactor(mixpanel): added temporary comment for tracker init

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -4,6 +4,11 @@ import ReactDOM from "react-dom/client"
 
 import App from "./App"
 
+// todo: add after Mixpanel token is received - M2-3002
+// import { Mixpanel } from "~/shared/utils"
+
+// Mixpanel.init()
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
