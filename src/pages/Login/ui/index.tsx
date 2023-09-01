db295c42787c4a4a779b128e4a50997169de42bf
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-09-01 23:05:46 +0300

    fix: Login / Signup

--- src/pages/Login/ui/index.tsx
+++ src/pages/Login/ui/index.tsx
@@ -15,27 +15,34 @@ export const LoginPage = () => {
 
   return (
     <PageContainer id="login-page" dataTestId="login-page">
-      <Box textAlign="center" margin="24px 0px" display="flex" alignItems="center" flexDirection="column">
-        <Box>
-          <Typography variant="h5" color={Theme.colors.light.primary}>
-            {t("welcomeMessage")}
-          </Typography>
-          <Typography variant="h5" color={Theme.colors.light.primary}>
-            {t("appType")}
-          </Typography>
+      <Box textAlign="center" display="flex" justifyContent="center" flex={1}>
+        <Box flex={1} margin="24px 0px">
+          <Box margin="24px 0px">
+            <Typography variant="h5" color={Theme.colors.light.primary}>
+              {t("welcomeMessage")}
+            </Typography>
+            <Typography variant="h5" color={Theme.colors.light.primary}>
+              {t("appType")}
+            </Typography>
+          </Box>
+
+          <Box
+            className="loginForm"
+            bgcolor={Theme.colors.light.onPrimary}
+            padding="10px"
+            maxWidth="400px"
+            margin="0 auto">
+            <LoginForm locationState={location.state} />
+
+            <BasicButton type="button" variant="outline-primary" className="mb-3" defaultSize>
+              <Link to={ROUTES.signup.path} relative="path">
+                {t("create")}
+              </Link>
+            </BasicButton>
+          </Box>
+
+          <DownloadMobileLinks />
         </Box>
-
-        <Box className="loginForm" width="400px">
-          <LoginForm locationState={location.state} />
-
-          <BasicButton type="button" variant="outline-primary" className="mb-3" defaultSize>
-            <Link to={ROUTES.signup.path} relative="path">
-              {t("create")}
-            </Link>
-          </BasicButton>
-        </Box>
-
-        <DownloadMobileLinks />
       </Box>
     </PageContainer>
   )
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -3,10 +3,8 @@ import Typography from "@mui/material/Typography"
 import { Link, useLocation } from "react-router-dom"
 
 import { SignupForm, useSignupTranslation } from "~/features/Signup"
-import { ROUTES } from "~/shared/constants"
+import { ROUTES, Theme } from "~/shared/constants"
 import { PageContainer } from "~/shared/ui"
-
-import "./styles.scss"
 
 export const SignupPage = () => {
   const { t } = useSignupTranslation()
@@ -14,15 +12,23 @@ export const SignupPage = () => {
 
   return (
     <PageContainer id="signup-page" dataTestId="signup-page">
-      <Box textAlign="center" margin="24px 0px" display="flex" alignItems="center" flexDirection="column">
-        <Typography variant="h4">{t("title")}</Typography>
+      <Box display="flex" justifyContent="center" flex={1}>
+        <Box textAlign="center" margin="24px 0px" flex={1}>
+          <Typography variant="h4" margin="24px 0px">
+            {t("title")}
+          </Typography>
 
-        <Box className="signupForm" width="400px">
-          <SignupForm locationState={location.state} />
-
-          <p className="my-3">
-            {t("account")} <Link to={ROUTES.login.path}> {t("logIn")}</Link>
-          </p>
+          <Box
+            className="signupForm"
+            maxWidth="400px"
+            padding="10px"
+            bgcolor={Theme.colors.light.onPrimary}
+            margin="0 auto">
+            <SignupForm locationState={location.state} />
+            <p className="my-3">
+              {t("account")} <Link to={ROUTES.login.path}> {t("logIn")}</Link>
+            </p>
+          </Box>
         </Box>
       </Box>
     </PageContainer>
