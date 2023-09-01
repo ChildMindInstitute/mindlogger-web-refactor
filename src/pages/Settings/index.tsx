d5eb775936c9c4805e6cb2890429fd92d80c8b07
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-09-01 15:28:35 +0300

    fix: Settings page layout

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,33 +1,39 @@
 import Box from "@mui/material/Box"
-import classNames from "classnames"
-import { Container } from "react-bootstrap"
+import Divider from "@mui/material/Divider"
+import Typography from "@mui/material/Typography"
 
 import { userModel } from "~/entities/user"
 import { ChangePasswordForm, useChangePasswordTranslation } from "~/features/ChangePassword"
-import { AvatarBase } from "~/shared/ui"
+import { Theme } from "~/shared/constants"
+import { AvatarBase, PageContainer } from "~/shared/ui"
 
 export const SettingsPage = () => {
   const { t } = useChangePasswordTranslation()
   const { user } = userModel.hooks.useUserState()
 
   return (
-    <div className="d-flex mp-3 align-self-start justify-content-center w-100 pt-3">
-      <div className="text-center my-2 px-3">
-        <div className="d-flex justify-content-start align-items-center">
-          <Box sx={{ padding: "0px 15px" }}>
-            <AvatarBase name={`${user?.firstName} ${user?.lastName}`} width="40px" height="40px" />
+    <PageContainer id="settings-page" dataTestId="settings-page">
+      <Box display="flex" justifyContent="center" alignItems="center" margin="24px 0px" textAlign="center">
+        <Box>
+          <Box display="flex" justifyContent="flex-start" alignItems="center">
+            <Box sx={{ padding: "0px 15px" }}>
+              <AvatarBase name={`${user?.firstName} ${user?.lastName}`} width="40px" height="40px" />
+            </Box>
+            <Typography variant="h6" marginBottom="8px">
+              {t("settings", { name: `${user?.firstName} ${user?.lastName}` })}
+            </Typography>
           </Box>
-          <h5>{t("settings", { name: `${user?.firstName} ${user?.lastName}` })}</h5>
-        </div>
-        <hr></hr>
 
-        <Container>
-          <h3 className={classNames("text-primary", "my-4")}>{t("settingsTitle")}</h3>
-        </Container>
-        <Container>
-          <ChangePasswordForm title={t("formTitle", { email: user?.email })} />
-        </Container>
-      </div>
-    </div>
+          <Divider sx={{ backgroundColor: "rgba(0,0,0, 0.5)", marginTop: "8px", marginBottom: "24px" }} />
+
+          <Box display="flex" alignItems="center" flexDirection="column">
+            <Typography variant="h5" color={Theme.colors.light.primary}>
+              {t("settingsTitle")}
+            </Typography>
+            <ChangePasswordForm title={t("formTitle", { email: user?.email })} />
+          </Box>
+        </Box>
+      </Box>
+    </PageContainer>
   )
 }
