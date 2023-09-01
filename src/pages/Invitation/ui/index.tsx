2f829a23950f022a4e6cfdb691a46ac80936bedd
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-09-01 16:40:12 +0300

    fix: Invitation page layout

--- src/pages/Invitation/ui/index.tsx
+++ src/pages/Invitation/ui/index.tsx
@@ -1,7 +1,7 @@
-import classNames from "classnames"
-import { Container } from "react-bootstrap"
+import Box from "@mui/material/Box"
 import { useLocation, useParams } from "react-router-dom"
 
+import { PageContainer } from "~/shared/ui"
 import { AuthorizationGuard } from "~/widgets/AuthorizationGuard"
 import { AuthorizationButtons } from "~/widgets/AuthorizationNavigateButtons"
 import { FetchInvitation } from "~/widgets/FetchInvitation"
@@ -16,13 +16,15 @@ const InvitationPage = () => {
   }
 
   return (
-    <Container className={classNames("mt-3", "pt-3")}>
-      {inviteId && (
-        <AuthorizationGuard fallback={<AuthorizationButtons redirectState={redirectState} />}>
-          <FetchInvitation keyParams={inviteId} />
-        </AuthorizationGuard>
-      )}
-    </Container>
+    <PageContainer id="invitation-page" dataTestId="invitation-page">
+      <Box margin="24px 0px">
+        {inviteId && (
+          <AuthorizationGuard fallback={<AuthorizationButtons redirectState={redirectState} />}>
+            <FetchInvitation keyParams={inviteId} />
+          </AuthorizationGuard>
+        )}
+      </Box>
+    </PageContainer>
   )
 }
 
