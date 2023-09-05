74b82bf9c671e6d6369218b039f1ea71f25fb126
Viktor Riabkov <vriabkov@scnsoft.com>
2023-09-05 12:42:01 +0300

    fix: after review

--- src/pages/PrivateJoin/index.tsx
+++ src/pages/PrivateJoin/index.tsx
@@ -1,7 +1,6 @@
 import Box from "@mui/material/Box"
 import { useLocation, useParams } from "react-router-dom"
 
-import {} from "~/shared/ui"
 import { FetchPrivateInvitation } from "~/widgets/FetchInvitation"
 
 export const PrivateJoinPage = () => {
