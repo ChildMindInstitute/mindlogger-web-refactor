b2ccf6510288ed9006fb92ec3d98809911216233
Viktor Riabkov <vriabkov@scnsoft.com>
2023-09-12 18:48:53 +0300

    fix: support media in markdown

---
+++ src/shared/ui/Markdown/lib/useMarkdownExtender.ts
@@ -0,0 +1,25 @@
+import { useCallback, useEffect, useState } from "react"
+
+import { markdownBuilder } from "./markdown-builder"
+
+export const useMarkdownExtender = (markdown: string) => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [extendedMarkdown, setExtendedMarkdown] = useState(markdown)
+
+  const extendMarkdown = useCallback(async (): Promise<string> => {
+    setIsLoading(true)
+    const processedMarkdown = await markdownBuilder.extend(markdown)
+    setIsLoading(false)
+    return processedMarkdown
+  }, [markdown])
+
+  useEffect(() => {
+    extendMarkdown().then(setExtendedMarkdown)
+  }, [extendMarkdown])
+
+  return {
+    isLoading,
+    markdown: extendedMarkdown,
+  }
+}
