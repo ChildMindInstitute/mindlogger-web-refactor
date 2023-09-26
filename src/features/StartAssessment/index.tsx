01fc5c41557d951f99745d1c0ec0df616a11c66a
Viktor Riabkov <vriabkov@scnsoft.com>
2023-09-27 00:01:57 +0300

    fix: button height and small refactoring

--- src/features/StartAssessment/index.tsx
+++ src/features/StartAssessment/index.tsx
@@ -1,8 +1,6 @@
 import Box from "@mui/material/Box"
-import ButtonBase from "@mui/material/ButtonBase"
 
-import { Theme } from "~/shared/constants"
-import { Text } from "~/shared/ui"
+import { BaseButton } from "~/shared/ui"
 import { useCustomTranslation } from "~/shared/utils"
 
 type Props = {
@@ -10,23 +8,14 @@ type Props = {
   onClick: () => void
 }
 
-export const StartAssessmentButton = ({ width, onClick }: Props) => {
+export const StartAssessmentButton = ({ onClick, width }: Props) => {
   const { t } = useCustomTranslation()
 
   return (
     <Box display="flex" flex={1} justifyContent="center">
-      <ButtonBase
-        onClick={onClick}
-        sx={{
-          padding: "10px 24px",
-          backgroundColor: Theme.colors.light.primary,
-          width,
-          borderRadius: "100px",
-        }}>
-        <Text color={Theme.colors.light.onPrimary} fontSize="14px" fontWeight="700">
-          {t("start")}
-        </Text>
-      </ButtonBase>
+      <Box width={width}>
+        <BaseButton type="button" variant="contained" onClick={onClick} text={t("start")} />
+      </Box>
     </Box>
   )
 }
