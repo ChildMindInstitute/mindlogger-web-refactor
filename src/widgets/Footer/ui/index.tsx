b7916a7b7afc480b09c77040470539ac8e8ee254
Viktor Riabkov <vriabkov@scnsoft.com>
2023-09-27 18:20:01 +0300

    fix: footer

--- src/widgets/Footer/ui/index.tsx
+++ src/widgets/Footer/ui/index.tsx
@@ -1,7 +1,6 @@
 import Box from "@mui/material/Box"
 
 import { Theme } from "~/shared/constants"
-import { Logo } from "~/shared/ui"
 
 import "./style.scss"
 
@@ -16,12 +15,13 @@ const Footer = () => {
       alignItems="center"
       sx={{ backgroundColor: Theme.colors.light.surface, borderTop: `1px solid ${Theme.colors.light.surfaceVariant}` }}>
       <div className="mt-3 mb-3 text-center d-flex align-items-center">
-        <Logo className="d-none d-sm-block" />
-        <span className="d-none d-sm-block">&#169;</span>
-        <a href="https://childmind.org" className="mx-1 mx-md-2" target="_blank" rel="noreferrer">
-          Child Mind Institute
-        </a>
-        <span className="d-none d-sm-block">2023</span>
+        <span className="d-none d-sm-block">
+          Mindlogger is a product of the{" "}
+          <a href="https://childmind.org" target="_blank" rel="noreferrer">
+            Child Mind Institute
+          </a>{" "}
+          &#169; 2023
+        </span>
         <a className="mx-1 mx-md-4" href="https://mindlogger.org/terms" target="_blank" rel="noreferrer">
           Terms of Service
         </a>
