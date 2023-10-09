f317993c79f22a152b4dbdeaacd19020febfb6cf
Viktor Riabkov <vriabkov@scnsoft.com>
2023-10-09 20:04:29 +0300

    fix: login after signup success

---
+++ src/entities/user/model/hooks/useOnLogin.ts
@@ -0,0 +1,46 @@
+import { useUserState } from "./useUserState"
+
+import { ROUTES } from "~/shared/constants"
+import { secureTokensStorage, secureUserPrivateKeyStorage, useCustomNavigation, useEncryption } from "~/shared/utils"
+
+type Params = {
+  isInvitationFlow?: boolean | undefined
+  backRedirectPath?: string | undefined
+}
+
+type OnLoginSuccessParams = {
+  user: {
+    id: string
+    email: string
+    firstName: string
+    lastName: string
+    password: string
+  }
+  tokens: {
+    accessToken: string
+    refreshToken: string
+    tokenType: string
+  }
+}
+
+export const useOnLogin = (params: Params) => {
+  const { navigate } = useCustomNavigation()
+  const { setUser } = useUserState()
+  const { generateUserPrivateKey } = useEncryption()
+
+  const onLoginSuccess = ({ user, tokens }: OnLoginSuccessParams) => {
+    const userPrivateKey = generateUserPrivateKey({ userId: user.id, email: user.email, password: user.password })
+    secureUserPrivateKeyStorage.setUserPrivateKey(userPrivateKey)
+
+    setUser({ id: user.id, email: user.email, firstName: user.firstName, lastName: user.lastName })
+    secureTokensStorage.setTokens(tokens)
+
+    if (params.isInvitationFlow) {
+      navigate(params.backRedirectPath as string)
+    } else {
+      navigate(ROUTES.appletList.path)
+    }
+  }
+
+  return { onLoginSuccess }
+}
