9a2a425fdf130453dd29bed2359741329ba116cd
Viktor Riabkov <vriabkov@scnsoft.com>
2023-12-05 20:36:54 +0200

    feature: Activity Groups builder refactored

---
+++ src/widgets/ActivityGroups/model/factories/ScheduledGroupEvaluator.ts
@@ -0,0 +1,86 @@
+import { EventEntity } from "../../lib"
+import { GroupUtility, GroupsBuildContext } from "./GroupUtility"
+
+import { IEvaluator } from "~/abstract/lib"
+import { AvailabilityLabelType, PeriodicityType } from "~/entities/event"
+import { DatesFromTo } from "~/shared/utils"
+
+export class ScheduledGroupEvaluator implements IEvaluator<EventEntity> {
+  private utility: GroupUtility
+
+  constructor(inputParams: GroupsBuildContext) {
+    this.utility = new GroupUtility(inputParams)
+  }
+
+  public evaluate(eventsEntities: Array<EventEntity>): Array<EventEntity> {
+    const notInProgress = eventsEntities.filter(x => !this.utility.isInProgress(x))
+
+    const result: Array<EventEntity> = []
+
+    const now = this.utility.getNow()
+
+    for (const eventEntity of notInProgress) {
+      const { event } = eventEntity
+
+      const isTypeScheduled = event.availability.availabilityType === AvailabilityLabelType.ScheduledAccess
+
+      const isAccessBeforeTimeFrom = event.availability.allowAccessBeforeFromTime
+
+      const isCompletedToday = this.utility.isCompletedToday(eventEntity)
+
+      const isScheduledToday = this.utility.isToday(event.scheduledAt)
+
+      const isSpreadToNextDay = this.utility.isSpreadToNextDay(event)
+
+      const isCandidateForBeingScheduled: boolean =
+        isTypeScheduled && isScheduledToday && now < event.scheduledAt! && !isAccessBeforeTimeFrom
+
+      if (!isCandidateForBeingScheduled) {
+        continue
+      }
+
+      if (!isSpreadToNextDay) {
+        !isCompletedToday && result.push(eventEntity)
+        continue
+      }
+
+      const periodicity = event.availability.periodicityType
+
+      const isMonday = now.getDay() === 1
+
+      const doSimpleSpreadCheck: boolean =
+        periodicity === PeriodicityType.Weekly ||
+        periodicity === PeriodicityType.Monthly ||
+        periodicity === PeriodicityType.Once ||
+        (periodicity === PeriodicityType.Weekdays && isMonday)
+
+      if (doSimpleSpreadCheck) {
+        !isCompletedToday && result.push(eventEntity)
+        continue
+      }
+
+      const isFromTueToFri = now.getDay() >= 2 && now.getDay() <= 5
+
+      const doAdvancedSpreadCheck =
+        periodicity === PeriodicityType.Daily || (periodicity === PeriodicityType.Weekdays && isFromTueToFri)
+
+      const considerSpread = doAdvancedSpreadCheck
+
+      const voidInterval: DatesFromTo = this.utility.getVoidTimeInterval(eventEntity.event, considerSpread)
+
+      const isInVoidInterval = this.utility.isInTimeInterval(voidInterval, now, "from")
+
+      const isCompletedInVoidInterval = this.utility.isInTimeInterval(
+        voidInterval,
+        this.utility.getCompletedAt(eventEntity),
+        "from",
+      )
+
+      if (doAdvancedSpreadCheck && isInVoidInterval && !isCompletedInVoidInterval) {
+        result.push(eventEntity)
+      }
+    }
+
+    return result
+  }
+}
