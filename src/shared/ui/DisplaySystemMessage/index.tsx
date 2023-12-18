21db363dd46bb0dc3d2bd28dc2caf0b21de116bb
Viktor Riabkov <vriabkov@scnsoft.com>
2023-12-18 19:32:13 +0200

    feature: DisplaySystemMessage change bootstrap to mui

--- src/shared/ui/DisplaySystemMessage/index.tsx
+++ src/shared/ui/DisplaySystemMessage/index.tsx
@@ -1,5 +1,4 @@
-import classNames from "classnames"
-import { Container } from "react-bootstrap"
+import { Container } from "../Container"
 
 import "./styles.scss"
 
@@ -10,12 +9,10 @@ interface ErrorLabelProps {
 
 export const DisplaySystemMessage = ({ errorMessage, successMessage }: ErrorLabelProps) => {
   return (
-    <Container className={classNames("system-message-box")}>
-      {errorMessage && <span className={classNames("system-message-label", "failed-message")}>{errorMessage}</span>}
+    <Container className="system-message-box">
+      {errorMessage && <span className="system-message-label failed-message">{errorMessage}</span>}
 
-      {successMessage && (
-        <span className={classNames("system-message-label", "success-message")}>{successMessage}</span>
-      )}
+      {successMessage && <span className="system-message-label success-message">{successMessage}</span>}
     </Container>
   )
 }
