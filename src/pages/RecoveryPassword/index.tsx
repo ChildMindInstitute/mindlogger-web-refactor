c3d575493e58f67fb1947cd48a0f7a01ce9c48d9
Viktor Riabkov <vriabkov@scnsoft.com>
2023-12-18 19:26:11 +0200

    feature: RecoveryPasswordPage change bootstrap to mui

--- src/pages/RecoveryPassword/index.tsx
+++ src/pages/RecoveryPassword/index.tsx
@@ -1,10 +1,8 @@
-import classNames from "classnames"
-import { Container } from "react-bootstrap"
 import { useSearchParams } from "react-router-dom"
 
 import { useRecoveryPasswordLinkHealthcheckQuery } from "~/entities/user"
 import { RecoveryPasswordForm, useRecoveryPasswordTranslation } from "~/features/RecoveryPassword"
-import { Loader, PageMessage } from "~/shared/ui"
+import { Container, Loader, PageMessage } from "~/shared/ui"
 
 export const RecoveryPasswordPage = () => {
   const [searchParams] = useSearchParams()
@@ -27,7 +25,7 @@ export const RecoveryPasswordPage = () => {
     <div className="d-flex mp-3 align-self-start justify-content-center w-100 pt-3">
       <div className="text-center my-2 px-3 w-100">
         <Container>
-          <h3 className={classNames("text-primary")}>{t("title")}</h3>
+          <h3 className="text-primary">{t("title")}</h3>
         </Container>
 
         <RecoveryPasswordForm title={t("formTitle", { email })} token={key} email={email} />
