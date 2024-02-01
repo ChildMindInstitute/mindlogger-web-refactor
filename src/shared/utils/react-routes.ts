14626dc19e0880c9007945118650df6cdc2e8430
Viktor Riabkov <vriabkov@scnsoft.com>
2024-02-02 00:30:53 +0200

    feat: matchPaths function

---
+++ src/shared/utils/react-routes.ts
@@ -0,0 +1,22 @@
+import { matchPath } from "react-router-dom"
+
+type Params = {
+  end?: boolean
+  caseSensitive?: boolean
+}
+
+export const matchPaths = (
+  patterns: string[],
+  pathname: string,
+  params: Params = { end: false, caseSensitive: false },
+) => {
+  return patterns.map(path =>
+    matchPath(
+      {
+        path,
+        ...params,
+      },
+      pathname,
+    ),
+  )
+}
