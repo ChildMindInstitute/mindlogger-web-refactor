57fee78e2aa2be2b09e49bac3fd0c6620771f248
sultanofcardio <kenroy.gobourne@gmail.com>
2024-02-01 02:13:37 +0000

    Simplify AdditionalTextResponse component

--- src/entities/activity/ui/AdditionalTextResponse.tsx
+++ src/entities/activity/ui/AdditionalTextResponse.tsx
@@ -6,21 +6,7 @@ type AdditionalTextResponseProps = {
 }
 
 export const AdditionalTextResponse = ({ value, onValueChange }: AdditionalTextResponseProps) => {
-  const onHandleValueChange = (value: string) => {
-    if (value.length === 0) {
-      return onValueChange("")
-    }
-
-    return onValueChange(value)
-  }
-
   return (
-    <TextField
-      fullWidth
-      size="small"
-      value={value}
-      onChange={e => onHandleValueChange(e.target.value)}
-      disabled={false}
-    />
+    <TextField fullWidth size="small" value={value} onChange={e => onValueChange(e.target.value)} disabled={false} />
   )
 }
