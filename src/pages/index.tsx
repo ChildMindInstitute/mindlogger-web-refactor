cdeee5dddc1217acfac7e40037132ef566077489
Viktor Riabkov <vriabkov@scnsoft.com>
2024-02-02 11:33:11 +0200

    feat: use useEntityProgressAutoCompletion in ApplicationRouter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,12 @@ const UnauthorizedRoutes = lazy(() => import("./UnauthorizedRoutes"))
 const AuthorizedRoutes = lazy(() => import("./AuthorizedRoutes"))
 
 import { userModel } from "~/entities/user"
+import { useEntityProgressAutoCompletion } from "~/features/EntityProgressAutoCompletion"
 
 function ApplicationRouter(): JSX.Element | null {
   const { isAuthorized, tokens } = userModel.hooks.useAuthorization()
+
+  useEntityProgressAutoCompletion()
 
   if (isAuthorized) {
     return <AuthorizedRoutes refreshToken={tokens.refreshToken} />
