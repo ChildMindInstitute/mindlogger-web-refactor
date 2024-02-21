d0ab3b9ca62aa511fbe3a34984a91a36a63860fc
sultanofcardio <kenroy.gobourne@gmail.com>
2024-02-21 14:22:18 +0000

    M2-4827: Add type import for EffectCallback

--- src/shared/utils/hooks/useOnceEffect.ts
+++ src/shared/utils/hooks/useOnceEffect.ts
@@ -1,10 +1,11 @@
 import { useEffect } from "react"
+import type { EffectCallback } from "react"
 
 /**
  * A convenience hook that runs the effect only once, when the component mounts.
  * @param effect Some code that you'd normally put in a `useEffect` hook.
  */
-export const useOnceEffect = (effect: React.EffectCallback) => {
+export const useOnceEffect = (effect: EffectCallback) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(effect, [])
 }
