e64eb6de955baf2823c673a1de6a0b5ac906c71f
Viktor Riabkov <vriabkov@scnsoft.com>
2024-02-22 13:17:14 +0800

    Apply base assessment layout to assessment welcome page

--- src/widgets/ActivityDetails/ui/AssessmentWelcomeScreen.tsx
+++ src/widgets/ActivityDetails/ui/AssessmentWelcomeScreen.tsx
@@ -3,9 +3,7 @@ import { useContext } from "react"
 import Box from "@mui/material/Box"
 
 import { ActivityMetaData } from "./ActivityMetaData"
-import { AssessmentLayoutFooter } from "./AssessmentLayoutFooter"
-import { AssessmentLayoutHeader } from "./AssessmentLayoutHeader"
-import Layout from "../../AppLayout"
+import { AssessmentLayout } from "./AssessmentLayout"
 import { ActivityDetailsContext } from "../lib"
 
 import { appletModel } from "~/entities/applet"
@@ -37,71 +35,61 @@ export const AssessmentWelcomeScreen = (props: Props) => {
   }
 
   return (
-    <Layout
-      bgColor={Theme.colors.light.surface}
-      header={
-        <AssessmentLayoutHeader
-          title={props.activityDetails.name}
-          progress={0}
-          appletId={context.appletId}
-          activityId={props.activityDetails.id}
-          eventId={context.eventId}
-          isPublic={context.isPublic}
-          publicKey={context.isPublic ? context.publicAppletKey : null}
+    <AssessmentLayout
+      activityName={props.activityDetails.name}
+      progress={0}
+      appletId={context.appletId}
+      activityId={props.activityDetails.id}
+      eventId={context.eventId}
+      isPublic={context.isPublic}
+      publicAppletKey={context.isPublic ? context.publicAppletKey : null}
+      footerActions={<StartAssessmentButton width={greaterThanSM ? "375px" : "335px"} onClick={startAssessment} />}>
+      <Box
+        id="welcome-screen-activity-details"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        marginTop="80px"
+        maxWidth="570px">
+        <AvatarBase
+          src={props.activityDetails.image}
+          name={props.activityDetails?.name}
+          width="124px"
+          height="124px"
+          testid="flow-welcome-screen-avatar"
         />
-      }
-      footer={
-        <AssessmentLayoutFooter>
-          <StartAssessmentButton width={greaterThanSM ? "375px" : "335px"} onClick={startAssessment} />
-        </AssessmentLayoutFooter>
-      }>
-      <Box height="100%" width="100%" display="flex" justifyContent="center" paddingTop="80px">
-        <Box
-          id="welcome-screen-activity-details"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          maxWidth="570px">
-          <AvatarBase
-            src={props.activityDetails.image}
-            name={props.activityDetails?.name}
-            width="124px"
-            height="124px"
-            testid="flow-welcome-screen-avatar"
+
+        <Text
+          variant="body1"
+          fontSize="18px"
+          fontWeight="400"
+          color={Theme.colors.light.secondary}
+          sx={{ marginTop: "24px" }}>
+          <ActivityMetaData
+            activityLength={props.activityDetails.items.length}
+            groupInProgress={getGroupProgress({ entityId, eventId: context.eventId })}
           />
+        </Text>
+        <Text
+          variant="body1"
+          fontSize="18px"
+          fontWeight="700"
+          color={Theme.colors.light.onSurface}
+          margin="16px 0px"
+          testid="flow-welcome-screen-title">
+          {props.activityDetails.name}
+        </Text>
 
-          <Text
-            variant="body1"
-            fontSize="18px"
-            fontWeight="400"
-            color={Theme.colors.light.secondary}
-            sx={{ marginTop: "24px" }}>
-            <ActivityMetaData
-              activityLength={props.activityDetails.items.length}
-              groupInProgress={getGroupProgress({ entityId, eventId: context.eventId })}
-            />
-          </Text>
-          <Text
-            variant="body1"
-            fontSize="18px"
-            fontWeight="700"
-            color={Theme.colors.light.onSurface}
-            margin="16px 0px"
-            testid="flow-welcome-screen-title">
-            {props.activityDetails.name}
-          </Text>
-
-          <Text
-            variant="body1"
-            fontSize="18px"
-            fontWeight="400"
-            color={Theme.colors.light.onSurface}
-            testid="flow-welcome-screen-decription"
-            sx={{ textAlign: "center" }}>
-            {props.activityDetails.description}
-          </Text>
-        </Box>
+        <Text
+          variant="body1"
+          fontSize="18px"
+          fontWeight="400"
+          color={Theme.colors.light.onSurface}
+          testid="flow-welcome-screen-decription"
+          sx={{ textAlign: "center" }}>
+          {props.activityDetails.description}
+        </Text>
       </Box>
-    </Layout>
+    </AssessmentLayout>
   )
 }
