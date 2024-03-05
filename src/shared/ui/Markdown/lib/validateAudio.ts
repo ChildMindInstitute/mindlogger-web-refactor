ef044f20f8e4389bc7fe82f6c13987210c1ae855
Viktor Riabkov <vriabkov@scnsoft.com>
2024-03-05 18:06:03 +0800

    Add support of .wav audio format

--- src/shared/ui/Markdown/lib/validateAudio.ts
+++ src/shared/ui/Markdown/lib/validateAudio.ts
@@ -1,5 +1,5 @@
 export function validateAudio(url: string): boolean {
-  const regex = new RegExp(/(?:((?:https|http):\/\/)|(?:\/)).+(?:.mp3)/gm);
+  const regex = new RegExp(/(?:((?:https|http):\/\/)|(?:\/)).+(?:.mp3|wav)/gm);
 
   return regex.test(url);
 }
