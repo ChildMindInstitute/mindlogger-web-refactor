7307f3f8f3cf66d3a889a78fc3940f1dee1a4f5f
Victor Ryabkov <45964820+moiskillnadne@users.noreply.github.com>
2024-03-29 18:34:09 +0800

    Enable mixpanel for UAT env (#417)
    Co-authored-by: Viktor Riabkov <vriabkov@scnsoft.com>

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,9 +4,10 @@ interface ImportMetaEnv {
   readonly VITE_API_HOST: string;
   readonly VITE_ADMIN_PANEL_HOST: string;
   readonly NODE_ENV: string;
-  readonly VITE_ENV: 'prod' | 'stage' | 'dev';
+  readonly VITE_ENV: 'prod' | 'stage' | 'dev' | 'uat';
   readonly VITE_BUILD_VERSION: string;
   readonly VITE_IV_LENGTH: string;
+  readonly VITE_MIXPANEL_TOKEN: string;
 }
 
 interface ImportMeta {
