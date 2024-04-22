adf1cd14ac93d3561d62993c62a967eea7094d23
sultanofcardio <kenroy.gobourne@gmail.com>
2024-04-22 11:16:30 -0500

    Revert "M2-6042: Temporary solution to multiple error messages showing"
    This reverts commit ec29433021acead7dfbca3be84499ba83c4010c9.

--- src/features/TakeNow/ui/ValidateTakeNowParams.tsx
+++ src/features/TakeNow/ui/ValidateTakeNowParams.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { useActivityByIdQuery } from '~/entities/activity';
 import { appletModel, useAppletByIdQuery } from '~/entities/applet';
@@ -55,25 +55,12 @@ function ValidateTakeNowParams({
     isLoading: isLoadingRespondent,
   } = useWorkspaceAppletRespondent(workspaceId, appletId, respondentId);
 
-  const { showErrorNotification: showError } = useNotification();
+  const { showErrorNotification } = useNotification();
   const { t } = useCustomTranslation();
   const { user } = useUserState();
   const { initiateTakeNow, getMultiInformantState } = appletModel.hooks.useMultiInformantState();
-  const errorNotificationRef = useRef<number>(0);
 
   const ActivityList = () => <ActivityGroups isPublic={false} appletId={appletId} />;
-
-  // This is a temporary hack to prevent showing many error notifications
-  // Working on a more permanent solution
-  const showErrorNotification = useCallback(
-    (message: string) => {
-      if (errorNotificationRef.current < 2) {
-        showError(message);
-        errorNotificationRef.current += 1;
-      }
-    },
-    [showError, errorNotificationRef],
-  );
 
   if (respondentId !== user.id) {
     // No impersonation allowed... yet?
