e7c24440b53f49981d2c393a4db7fb713679d47e
Viktor Riabkov <vriabkov@scnsoft.com>
2024-04-29 13:19:52 +0400

    The dsn import from the env was covered by guard

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 import AppSuspense from './AppSuspense';
 
 Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
+  dsn: import.meta.env.VITE_SENTRY_DSN ?? '',
   integrations: [
     // See docs for support of different versions of variation of react router
     // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
