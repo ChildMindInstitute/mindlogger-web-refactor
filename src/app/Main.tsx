f7977adb25c983cbbc7015e9dae1df5ca4020bce
Viktor Riabkov <vriabkov@scnsoft.com>
2024-04-29 13:16:06 +0400

    Sentry setup

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
+import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom/client';
 
 import AppSuspense from './AppSuspense';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    // See docs for support of different versions of variation of react router
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+    Sentry.replayIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  tracePropagationTargets: import.meta.env.VITE_SENTRY_TRACE_PROPAGATION_TARGETS
+    ? (JSON.parse(import.meta.env.VITE_SENTRY_TRACE_PROPAGATION_TARGETS) as Array<string>)
+    : [],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
