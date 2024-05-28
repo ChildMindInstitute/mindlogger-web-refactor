e49b8225b4eb4b823a5344d642778da7197c71e4
Viktor Riabkov <vriabkov@scnsoft.com>
2024-05-28 12:43:18 +0300

    The summary screen data-testids coverage

--- src/features/PassSurvey/ui/SummaryScreen/Alerts.tsx
+++ src/features/PassSurvey/ui/SummaryScreen/Alerts.tsx
@@ -13,18 +13,24 @@ export const Alerts = ({ alerts }: Props) => {
   const { t } = useCustomTranslation();
 
   return (
-    <Box padding="24px 32px" bgcolor={Theme.colors.light.errorVariant10} borderRadius="12px">
-      <Text fontWeight="600" fontSize="24px" lineHeight="32px">
+    <Box
+      padding="24px 32px"
+      bgcolor={Theme.colors.light.errorVariant10}
+      borderRadius="12px"
+      data-testid="alerts-container"
+    >
+      <Text fontWeight="600" fontSize="24px" lineHeight="32px" testid="alerts-title">
         {t('alerts')}
       </Text>
       <Box>
         {alerts.map((alert, index) => (
-          <Box display="flex" gap="8px" key={index} marginTop="16px">
+          <Box display="flex" gap="8px" key={index} marginTop="16px" data-testid="alert-item">
             <NotificationImportantIcon
               fontSize="small"
               sx={{ color: Theme.colors.light.errorVariant100 }}
+              data-testid="alert-item-icon"
             />
-            <Text fontWeight="400" fontSize="16px" lineHeight="22px">
+            <Text fontWeight="400" fontSize="16px" lineHeight="22px" testid="alert-item-label">
               {alert}
             </Text>
           </Box>
--- src/features/PassSurvey/ui/SummaryScreen/Scores.tsx
+++ src/features/PassSurvey/ui/SummaryScreen/Scores.tsx
@@ -14,12 +14,19 @@ export const Scores = (props: Props) => {
   return (
     <Box display="flex" gap="16px" flexDirection="column">
       {props.scores.map((score, index) => (
-        <Box key={index} display="flex" justifyContent="space-between" alignItems="center">
+        <Box
+          key={index}
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          data-testid="score-item"
+        >
           <Box display="flex" gap="8px" alignItems="center">
             {score.highlighted && (
               <NotificationImportantIcon
                 fontSize="small"
                 sx={{ color: Theme.colors.light.errorVariant100 }}
+                data-testid="score-item-highlighted-icon"
               />
             )}
             <Text
@@ -27,6 +34,7 @@ export const Scores = (props: Props) => {
               fontSize="18px"
               lineHeight="24px"
               color={score.highlighted ? Theme.colors.light.errorVariant100 : undefined}
+              testid="score-item-label"
             >
               {score.label}
             </Text>
@@ -38,8 +46,9 @@ export const Scores = (props: Props) => {
             padding="4px 0px"
             bgcolor={score.highlighted ? Theme.colors.light.errorVariant10 : undefined}
             borderRadius="100px"
+            data-testid="score-item-value-container"
           >
-            <Text fontWeight="300" fontSize="28px" lineHeight="38px">
+            <Text fontWeight="300" fontSize="28px" lineHeight="38px" testid="score-item-value">
               {score.value}
             </Text>
           </Box>
