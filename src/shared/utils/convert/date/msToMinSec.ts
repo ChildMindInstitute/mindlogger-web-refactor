7908e7b0a2e583e768f49946afc7ed5af6747012
Viktor Riabkov <vriabkov@scnsoft.com>
2024-06-01 12:33:58 +0300

    time converted was changed

--- src/shared/utils/convert/date/msToMinSec.ts
+++ src/shared/utils/convert/date/msToMinSec.ts
@@ -2,9 +2,13 @@ export function convertMillisecondsToMinSec(milliseconds: number): string {
   // Calculate total seconds from milliseconds
   const totalSeconds = Math.floor(milliseconds / 1000);
 
+  return convertSecondsToMinSec(totalSeconds);
+}
+
+export function convertSecondsToMinSec(value: number) {
   // Calculate minutes and seconds
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
+  const minutes = Math.floor(value / 60);
+  const seconds = value % 60;
 
   // Format seconds to always have two digits
   const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
