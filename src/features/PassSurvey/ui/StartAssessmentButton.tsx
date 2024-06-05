56627918f57ed5302b81ecf6970d95b1fcdffc87
Victor Ryabkov <45964820+moiskillnadne@users.noreply.github.com>
2024-06-05 16:26:11 +0300

    No Summary screen after flow execution if the checkbox "Show Score Summary" is disable in at least one activity in the flow(fix:M2-6778)  (#472)
    * Footer component on the AssessmentWelcomeScreen was fixed
    
    * Change condition in the onSubmitAnswer method
    
    ---------
    
    Co-authored-by: Viktor Riabkov <vriabkov@scnsoft.com>

--- src/features/PassSurvey/ui/StartAssessmentButton.tsx
+++ src/features/PassSurvey/ui/StartAssessmentButton.tsx
@@ -11,7 +11,13 @@ export const StartAssessmentButton = ({ onClick, width }: Props) => {
   const { t } = useCustomTranslation();
 
   return (
-    <Box display="flex" flex={1} justifyContent="center" data-testid="start-assessment-widget">
+    <Box
+      display="flex"
+      flex={1}
+      justifyContent="center"
+      data-testid="start-assessment-widget"
+      paddingY="23px"
+    >
       <Box width={width}>
         <BaseButton type="button" variant="contained" onClick={onClick} text={t('start')} />
       </Box>
