453731570711209b0c5c61193d85514c6c075e46
sultanofcardio <kenroy.gobourne@gmail.com>
2024-06-11 11:45:18 -0500

    Update the link to the Google Play Store

--- src/abstract/lib/constants.ts
+++ src/abstract/lib/constants.ts
@@ -1,6 +1,6 @@
 export const APPSTORE_LINK = 'https://apps.apple.com/app/mindlogger/id1299242097';
 export const GOOGLEPLAY_LINK =
-  'https://play.google.com/store/apps/details?id=lab.childmindinstitute.data';
+  'https://play.google.com/store/apps/details?id=lab.childmindinstitute.data.production';
 
 export const supportableResponseTypes = [
   'text',
