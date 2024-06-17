c54db1c6e94f37315401642a3146f809a0c9ce66
Viktor Riabkov <vriabkov@scnsoft.com>
2024-06-17 18:22:09 +0300

    FlowId was added to the SurveyBasicContext

--- src/pages/PublicActivityDetails/index.tsx
+++ src/pages/PublicActivityDetails/index.tsx
@@ -1,12 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 import Box from '~/shared/ui/Box';
 import { useCustomTranslation } from '~/shared/utils';
 import { SurveyBasicContext, SurveyWidget } from '~/widgets/Survey';
 
 function PublicActivityDetailsPage() {
-  const { appletId, activityId, eventId, publicAppletKey } = useParams();
   const { t } = useCustomTranslation();
+
+  const { appletId, activityId, eventId, publicAppletKey, entityType } = useParams();
+
+  const [searchParams] = useSearchParams();
+
+  const isFlow = entityType === 'flow';
+
+  let flowId: string | null = null;
+
+  if (isFlow) {
+    flowId = searchParams.get('flowId');
+  }
 
   if (!appletId || !activityId || !eventId || !publicAppletKey) {
     return <div>{t('wrondLinkParametrError')}</div>;
@@ -15,7 +26,7 @@ function PublicActivityDetailsPage() {
   return (
     <Box display="flex" flex={1}>
       <SurveyBasicContext.Provider
-        value={{ appletId, activityId, eventId, publicAppletKey, isPublic: true }}
+        value={{ appletId, activityId, eventId, publicAppletKey, flowId, isPublic: true }}
       >
         <SurveyWidget />
       </SurveyBasicContext.Provider>
--- src/pages/Survey/index.tsx
+++ src/pages/Survey/index.tsx
@@ -1,12 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 import Box from '~/shared/ui/Box';
 import { useCustomTranslation } from '~/shared/utils';
 import { SurveyBasicContext, SurveyWidget } from '~/widgets/Survey';
 
 function SurveyPage() {
-  const { appletId, activityId, eventId } = useParams();
   const { t } = useCustomTranslation();
+
+  const { appletId, activityId, eventId, entityType } = useParams();
+
+  const [searchParams] = useSearchParams();
+
+  const isFlow = entityType === 'flow';
+
+  let flowId: string | null = null;
+
+  if (isFlow) {
+    flowId = searchParams.get('flowId');
+  }
 
   if (!appletId || !activityId || !eventId) {
     return <div>{t('wrondLinkParametrError')}</div>;
@@ -14,7 +25,9 @@ function SurveyPage() {
 
   return (
     <Box display="flex" flex={1}>
-      <SurveyBasicContext.Provider value={{ appletId, activityId, eventId, isPublic: false }}>
+      <SurveyBasicContext.Provider
+        value={{ appletId, activityId, eventId, flowId, isPublic: false }}
+      >
         <SurveyWidget />
       </SurveyBasicContext.Provider>
     </Box>
