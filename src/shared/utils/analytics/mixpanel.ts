20ad1cadbd152651680b72266029cdd976f6ba7f
farmerpaul <paul.hh@metalab.com>
2024-06-20 16:41:43 -0300

    feat: allow events tracking in dev env for testing
    Also include app build # in Mixpanel user profile.

--- src/shared/utils/analytics/mixpanel.ts
+++ src/shared/utils/analytics/mixpanel.ts
@@ -5,12 +5,17 @@ const PROJECT_TOKEN = import.meta.env.VITE_MIXPANEL_TOKEN;
 const isProduction = import.meta.env.VITE_ENV === 'prod';
 const isStaging = import.meta.env.VITE_ENV === 'stage';
 const isUat = import.meta.env.VITE_ENV === 'uat';
+const isDev = import.meta.env.VITE_ENV === 'dev';
+const isJest = !!import.meta.env.JEST_WORKER_ID;
 
-const shouldEnableMixpanel = PROJECT_TOKEN && (isProduction || isStaging || isUat);
+const shouldEnableMixpanel =
+  PROJECT_TOKEN &&
+  !isJest &&
+  (isProduction || isStaging || isUat || import.meta.env.VITE_MIXPANEL_FORCE_ENABLE === 'true');
 
 export const Mixpanel = {
   init() {
-    if (shouldEnableMixpanel) mixpanel.init(PROJECT_TOKEN);
+    if (shouldEnableMixpanel) mixpanel.init(PROJECT_TOKEN, { ignore_dnt: isDev });
   },
   trackPageView(pageName: string) {
     if (shouldEnableMixpanel) mixpanel.track_pageview({ page: `[Web] ${pageName}` });
@@ -21,7 +26,10 @@ export const Mixpanel = {
   login(userId: string) {
     if (shouldEnableMixpanel) {
       mixpanel.identify(userId);
-      mixpanel.people.set({ 'User ID': userId });
+      mixpanel.people.set({
+        'User ID': userId,
+        'App Build Number': import.meta.env.VITE_BUILD_VERSION,
+      });
     }
   },
   logout() {
