fc78c54aedde00701f8eb8f41c919ecbac6fe933
Viktor Riabkov <vriabkov@scnsoft.com>
2024-07-09 15:05:31 +0400

    Wrapped all method in useCallback in the useEntityComplete hook

--- src/entities/applet/model/hooks/useEntityComplete.ts
+++ src/entities/applet/model/hooks/useEntityComplete.ts
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 import type { NavigateOptions } from 'react-router/dist/lib/context';
 
 import { ActivityPipelineType } from '~/abstract/lib';
@@ -36,113 +38,143 @@ export const useEntityComplete = (props: Props) => {
   const { entityCompleted, flowUpdated, getGroupProgress } =
     appletModel.hooks.useGroupProgressState();
 
-  const completeEntityAndRedirect = (completionType: CompletionType) => {
-    entityCompleted({
-      entityId: props.flowId ? props.flowId : props.activityId,
-      eventId: props.eventId,
-    });
-
-    const isAutoCompletion = completionType === 'autoCompletion';
-
-    if (isAutoCompletion) {
-      return;
-    }
-
-    if (props.publicAppletKey) {
-      return navigator.navigate(ROUTES.publicJoin.navigateTo(props.publicAppletKey), {
-        replace: true,
+  const completeEntityAndRedirect = useCallback(
+    (completionType: CompletionType) => {
+      entityCompleted({
+        entityId: props.flowId ? props.flowId : props.activityId,
+        eventId: props.eventId,
       });
-    }
 
-    const navigateOptions: NavigateOptions = {
-      replace: true,
-    };
+      const isAutoCompletion = completionType === 'autoCompletion';
 
-    if (featureFlags.enableMultiInformant && isInMultiInformantFlow()) {
-      navigateOptions.state = { showTakeNowSuccessModal: true };
-    }
+      if (isAutoCompletion) {
+        return;
+      }
 
-    return navigator.navigate(ROUTES.appletDetails.navigateTo(props.appletId), navigateOptions);
-  };
+      if (props.publicAppletKey) {
+        return navigator.navigate(ROUTES.publicJoin.navigateTo(props.publicAppletKey), {
+          replace: true,
+        });
+      }
 
-  const redirectToNextActivity = (activityId: string) => {
-    if (props.publicAppletKey) {
+      const navigateOptions: NavigateOptions = {
+        replace: true,
+      };
+
+      if (featureFlags.enableMultiInformant && isInMultiInformantFlow()) {
+        navigateOptions.state = { showTakeNowSuccessModal: true };
+      }
+
+      return navigator.navigate(ROUTES.appletDetails.navigateTo(props.appletId), navigateOptions);
+    },
+    [
+      entityCompleted,
+      featureFlags.enableMultiInformant,
+      isInMultiInformantFlow,
+      navigator,
+      props.activityId,
+      props.appletId,
+      props.eventId,
+      props.flowId,
+      props.publicAppletKey,
+    ],
+  );
+
+  const redirectToNextActivity = useCallback(
+    (activityId: string) => {
+      if (props.publicAppletKey) {
+        return navigator.navigate(
+          ROUTES.publicSurvey.navigateTo({
+            appletId: props.appletId,
+            activityId,
+            eventId: props.eventId,
+            entityType: 'flow',
+            publicAppletKey: props.publicAppletKey,
+            flowId: props.flowId,
+          }),
+          { replace: true },
+        );
+      }
+
       return navigator.navigate(
-        ROUTES.publicSurvey.navigateTo({
+        ROUTES.survey.navigateTo({
           appletId: props.appletId,
           activityId,
           eventId: props.eventId,
           entityType: 'flow',
-          publicAppletKey: props.publicAppletKey,
           flowId: props.flowId,
         }),
         { replace: true },
       );
-    }
+    },
+    [navigator, props.appletId, props.eventId, props.flowId, props.publicAppletKey],
+  );
 
-    return navigator.navigate(
-      ROUTES.survey.navigateTo({
-        appletId: props.appletId,
-        activityId,
+  const completeFlow = useCallback(
+    (input?: CompleteOptions) => {
+      const isAutoCompletion = input?.type === 'autoCompletion';
+
+      const groupProgress = getGroupProgress({
+        entityId: props.flowId ? props.flowId : props.activityId,
         eventId: props.eventId,
-        entityType: 'flow',
-        flowId: props.flowId,
-      }),
-      { replace: true },
-    );
-  };
+      });
 
-  const completeFlow = (input?: CompleteOptions) => {
-    const { flow } = props;
+      if (!groupProgress) {
+        return;
+      }
 
-    const isAutoCompletion = input?.type === 'autoCompletion';
+      const isFlow = groupProgress.type === ActivityPipelineType.Flow;
 
-    const groupProgress = getGroupProgress({
-      entityId: props.flowId ? props.flowId : props.activityId,
-      eventId: props.eventId,
-    });
+      if (!isFlow) {
+        return;
+      }
 
-    if (!groupProgress) {
-      return;
-    }
+      const currentPipelineActivityOrder = groupProgress.pipelineActivityOrder;
 
-    const isFlow = groupProgress.type === ActivityPipelineType.Flow;
+      if (!props.flow) {
+        throw new Error('[UseEntityComplete:completeFlow] Flow not found');
+      }
 
-    if (!isFlow) {
-      return;
-    }
+      const nextActivityId = props.flow.activityIds[currentPipelineActivityOrder + 1];
 
-    const currentPipelineActivityOrder = groupProgress.pipelineActivityOrder;
+      flowUpdated({
+        activityId: nextActivityId ? nextActivityId : props.flow.activityIds[0],
+        flowId: props.flow.id,
+        eventId: props.eventId,
+        pipelineActivityOrder: nextActivityId ? currentPipelineActivityOrder + 1 : 0,
+      });
 
-    if (!flow) {
-      throw new Error('[UseEntityComplete:completeFlow] Flow not found');
-    }
+      removeActivityProgress({ activityId: props.activityId, eventId: props.eventId });
 
-    const nextActivityId = flow.activityIds[currentPipelineActivityOrder + 1];
+      if (nextActivityId && !isAutoCompletion) {
+        return redirectToNextActivity(nextActivityId);
+      }
 
-    flowUpdated({
-      activityId: nextActivityId ? nextActivityId : flow.activityIds[0],
-      flowId: flow.id,
-      eventId: props.eventId,
-      pipelineActivityOrder: nextActivityId ? currentPipelineActivityOrder + 1 : 0,
-    });
+      if (!nextActivityId) {
+        return completeEntityAndRedirect(input?.type || 'regular');
+      }
+    },
+    [
+      completeEntityAndRedirect,
+      flowUpdated,
+      getGroupProgress,
+      props.activityId,
+      props.eventId,
+      props.flow,
+      props.flowId,
+      redirectToNextActivity,
+      removeActivityProgress,
+    ],
+  );
 
-    removeActivityProgress({ activityId: props.activityId, eventId: props.eventId });
+  const completeActivity = useCallback(
+    (input?: CompleteOptions) => {
+      removeActivityProgress({ activityId: props.activityId, eventId: props.eventId });
 
-    if (nextActivityId && !isAutoCompletion) {
-      return redirectToNextActivity(nextActivityId);
-    }
-
-    if (!nextActivityId) {
       return completeEntityAndRedirect(input?.type || 'regular');
-    }
-  };
-
-  const completeActivity = (input?: CompleteOptions) => {
-    removeActivityProgress({ activityId: props.activityId, eventId: props.eventId });
-
-    return completeEntityAndRedirect(input?.type || 'regular');
-  };
+    },
+    [completeEntityAndRedirect, props.activityId, props.eventId, removeActivityProgress],
+  );
 
   return {
     completeActivity,
