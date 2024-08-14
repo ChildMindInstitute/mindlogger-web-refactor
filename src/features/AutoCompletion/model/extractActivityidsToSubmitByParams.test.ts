f4598d6254d283e32ad37c1caa4284083ff42d1c
Victor Ryabkov <45964820+moiskillnadne@users.noreply.github.com>
2024-08-14 20:05:09 +0300

    (fix:M2-7597) The extractActivityIdsToSubmit has incorrect business logic (#524)
    * Remove activityProgress after autoCompletion
    
    * Fix: clearTimer when useEffect unmount phase
    
    * fix: M2-7353
    
    * Fix the activity ids to submit calculcation logic
    
    ---------
    
    Co-authored-by: Viktor Riabkov <vriabkov@scnsoft.com>

--- src/features/AutoCompletion/model/extractActivityidsToSubmitByParams.test.ts
+++ src/features/AutoCompletion/model/extractActivityidsToSubmitByParams.test.ts
@@ -1,11 +1,23 @@
 import { extractActivityIdsToSubmitByParams } from './extractActivityidsToSubmitByParams';
 
+import { appletModel } from '~/entities/applet';
+
 describe('extractActivityIdsToSubmitByParams', () => {
+  const mockActivityProgress: appletModel.ActivityProgress = {
+    items: [],
+    step: 0,
+    userEvents: [],
+    isSummaryScreenOpen: false,
+    scoreSettings: undefined,
+    itemTimer: {},
+  };
+
   it('should return an array with the current activity id if isFlow is false', () => {
     const params = {
       isFlow: false,
-      currentActivityId: 'activity1',
+      interruptedActivityId: 'activity1',
       flowActivityIds: null,
+      interruptedActivityProgress: mockActivityProgress,
     };
 
     const result = extractActivityIdsToSubmitByParams(params);
@@ -16,8 +28,9 @@ describe('extractActivityIdsToSubmitByParams', () => {
   it('should throw an error if flowActivityIds is not defined', () => {
     const params = {
       isFlow: true,
-      currentActivityId: 'activity1',
+      interruptedActivityId: 'activity1',
       flowActivityIds: null,
+      interruptedActivityProgress: mockActivityProgress,
     };
 
     expect(() => extractActivityIdsToSubmitByParams(params)).toThrowError(
@@ -28,8 +41,9 @@ describe('extractActivityIdsToSubmitByParams', () => {
   it('should return an array with the current activity id and the last activity id if isInterruptedActivityLast is false', () => {
     const params = {
       isFlow: true,
-      currentActivityId: 'activity1',
+      interruptedActivityId: 'activity1',
       flowActivityIds: ['activity1', 'activity2', 'activity3'],
+      interruptedActivityProgress: mockActivityProgress,
     };
 
     const result = extractActivityIdsToSubmitByParams(params);
@@ -40,8 +54,9 @@ describe('extractActivityIdsToSubmitByParams', () => {
   it('should return an array with only the current activity id if isInterruptedActivityLast is true', () => {
     const params = {
       isFlow: true,
-      currentActivityId: 'activity3',
+      interruptedActivityId: 'activity3',
       flowActivityIds: ['activity1', 'activity2', 'activity3'],
+      interruptedActivityProgress: mockActivityProgress,
     };
 
     const result = extractActivityIdsToSubmitByParams(params);
@@ -52,12 +67,52 @@ describe('extractActivityIdsToSubmitByParams', () => {
   it('should return an array with the current activity id and the last activity id if isInterruptedActivityLast is false', () => {
     const params = {
       isFlow: true,
-      currentActivityId: 'activity2',
+      interruptedActivityId: 'activity2',
       flowActivityIds: ['activity1', 'activity2', 'activity3'],
+      interruptedActivityProgress: mockActivityProgress,
     };
 
     const result = extractActivityIdsToSubmitByParams(params);
 
     expect(result).toEqual(['activity2', 'activity3']);
+  });
+
+  it('should return an array with the last activity id if is interrupted activity id has not progress state', () => {
+    const params = {
+      isFlow: true,
+      interruptedActivityId: 'activity2',
+      flowActivityIds: ['activity1', 'activity2', 'activity3'],
+      interruptedActivityProgress: null,
+    };
+
+    const result = extractActivityIdsToSubmitByParams(params);
+
+    expect(result).toEqual(['activity3']);
+  });
+
+  it('should return an array with the current activity id and the last activity id if isInterruptedActivityLast is false', () => {
+    const params = {
+      isFlow: true,
+      interruptedActivityId: 'activity2',
+      flowActivityIds: ['activity1', 'activity2', 'activity3'],
+      interruptedActivityProgress: mockActivityProgress,
+    };
+
+    const result = extractActivityIdsToSubmitByParams(params);
+
+    expect(result).toEqual(['activity2', 'activity3']);
+  });
+
+  it('should return an array with the last activity id if the interrupted activity progress is empty', () => {
+    const params = {
+      isFlow: true,
+      interruptedActivityId: 'activity2',
+      flowActivityIds: ['activity1', 'activity2', 'activity3'],
+      interruptedActivityProgress: null,
+    };
+
+    const result = extractActivityIdsToSubmitByParams(params);
+
+    expect(result).toEqual(['activity3']);
   });
 });
