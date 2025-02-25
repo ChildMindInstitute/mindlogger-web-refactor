9c37c8efd00d864150cc55c2851671c02240106e
farmerpaul <paul.hh@metalab.com>
2025-02-25 12:23:19 -0400

    feat: Submit `eventHistoryId` in answer data

--- src/features/PassSurvey/model/AnswersConstructService.ts
+++ src/features/PassSurvey/model/AnswersConstructService.ts
@@ -142,6 +142,7 @@ export default class AnswersConstructService implements ICompletionConstructServ
         width: window.innerWidth,
         height: window.innerHeight,
       },
+      eventHistoryId: `${this.event.id}_${this.event.version}`,
     };
   }
 
