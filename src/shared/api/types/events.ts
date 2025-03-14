82e2530cb61b40886d567dce67e72a2dce516cb1
farmerpaul <paul.hh@metalab.com>
2025-03-14 15:26:33 -0300

    chore: remove confusing unused type definitions

--- src/shared/api/types/events.ts
+++ src/shared/api/types/events.ts
@@ -1,6 +1,5 @@
 import { AllUserEventsDTO, AppletEventsResponse } from './applet';
 import { BaseSuccessResponse } from './base';
-import { HourMinute } from '../../utils';
 
 export type TimerTypeDTO = 'NOT_SET';
 export type PeriodicityTypeDTO = 'ONCE' | 'DAILY' | 'WEEKLY' | 'WEEKDAYS' | 'MONTHLY' | 'ALWAYS';
@@ -15,28 +14,3 @@ export type GetEventsByPublicAppletKey = {
 
 export type SuccessEventsByAppletIdResponse = BaseSuccessResponse<AppletEventsResponse>;
 export type SuccessAllUserEventsResponse = BaseSuccessResponse<AllUserEventsDTO[]>;
-
-export type EventsByAppletIdResponseDTO = {
-  appletId: string;
-  events: EventDTO[];
-};
-
-export type EventDTO = {
-  id: string;
-  entityId: string;
-  availability: {
-    oneTimeCompletion: boolean;
-    periodicityType: 'ONCE' | 'DAILY' | 'WEEKLY' | 'WEEKDAYS' | 'MONTHLY' | 'ALWAYS';
-    timeFrom: HourMinute;
-    timeTo: HourMinute;
-    allowAccessBeforeFromTime: boolean;
-    startDate: string;
-    endDate: string;
-  };
-  selectedDate: string | null;
-  timers: {
-    timer: HourMinute;
-    idleTimer: HourMinute;
-  };
-  availabilityType: 'AlwaysAvailable' | 'ScheduledAccess';
-};
