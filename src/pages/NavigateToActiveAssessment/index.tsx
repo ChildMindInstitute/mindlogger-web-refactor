ad60619752851475d2d70b222c1ef5b3804381ac
farmerpaul <paul.hh@metalab.com>
2025-04-23 16:21:12 -0300

    feat: `/active-assessment` nav to EHR last substep
    When the current assessment is being opened via the `/active-assessment`
    route, check if there's an EHR in this activity, and the EHR is the
    currently active step, and the 1UpHealth iframe is the currently active
    substep. If so, skip to the last step, because it's presumed the only
    time this will happen is when this route is called by the 3rd-party
    health data provider, under those specific circumstances.

--- src/pages/NavigateToActiveAssessment/index.tsx
+++ src/pages/NavigateToActiveAssessment/index.tsx
@@ -1,36 +1,85 @@
 import { Navigate } from 'react-router-dom';
 
-import { ActivityPipelineType, getDataFromProgressId } from '~/abstract/lib';
+import { ActivityPipelineType, getDataFromProgressId, getProgressId } from '~/abstract/lib';
+import { RequestHealthRecordDataItemStep } from '~/entities/activity';
+import { appletModel } from '~/entities/applet';
+import { actions } from '~/entities/applet/model';
 import { useActiveAssessment } from '~/entities/applet/model/hooks';
 import { selectGroupProgress } from '~/entities/applet/model/selectors';
 import ROUTES from '~/shared/constants/routes';
-import { useAppSelector } from '~/shared/utils';
+import { useAppDispatch, useAppSelector } from '~/shared/utils';
 
 const NavigateToActiveAssessment = () => {
+  const dispatch = useAppDispatch();
   const { activeAssessment } = useActiveAssessment();
 
   const groupProgress = useAppSelector((state) => {
     if (!activeAssessment?.groupProgressId) return null;
-
     return selectGroupProgress(state, activeAssessment.groupProgressId);
   });
 
-  if (!activeAssessment || !groupProgress) {
-    // Navigate to applets list if no in-progress assessment
+  const progressData = activeAssessment?.groupProgressId
+    ? getDataFromProgressId(activeAssessment.groupProgressId)
+    : null;
+
+  const activityId =
+    groupProgress &&
+    progressData &&
+    (groupProgress.type === ActivityPipelineType.Flow
+      ? groupProgress.currentActivityId
+      : progressData.entityId);
+
+  const activityProgress = useAppSelector((state) => {
+    if (!activityId) return null;
+
+    return appletModel.selectors.selectActivityProgress(
+      state,
+      getProgressId(activityId, progressData.eventId, progressData.targetSubjectId),
+    );
+  });
+
+  /* No active assessment found, navigate to applet list
+  =================================================== */
+  if (!activeAssessment || !activityId) {
     return <Navigate to={ROUTES.appletList.path} />;
   }
 
-  const { entityId, eventId, targetSubjectId } = getDataFromProgressId(
-    activeAssessment.groupProgressId,
-  );
+  /* Specific handling of activities containing EHR item types:
+   *
+   * When the activity contains an EHR item type that's in the OneUpHealth step, and we're resuming
+   * the activity via the /active-assessment route, advance the item's state to the last sub-step
+   * (after the OneUpHealth step).
+   =================================================== */
+  const ehrItemIndex =
+    activityProgress?.items.findIndex((item) => item.responseType === 'requestHealthRecordData') ??
+    -1;
+  const ehrItem = ehrItemIndex >= 0 ? activityProgress?.items[ehrItemIndex] : null;
 
+  if (
+    ehrItem &&
+    activityProgress?.step === ehrItemIndex &&
+    ehrItem.subStep === RequestHealthRecordDataItemStep.OneUpHealth
+  ) {
+    dispatch(
+      actions.saveCustomProperty({
+        entityId: activityId,
+        eventId: progressData.eventId,
+        targetSubjectId: progressData.targetSubjectId,
+        itemId: ehrItem.id,
+        customProperty: 'subStep',
+        value: RequestHealthRecordDataItemStep.AdditionalPrompt,
+      }),
+    );
+  }
+
+  /* Navigate to active assessment
+  =================================================== */
   const navigateParams = {
     appletId: activeAssessment.appletId,
-    activityId:
-      groupProgress.type === ActivityPipelineType.Flow ? groupProgress.currentActivityId : entityId,
+    activityId,
     entityType: groupProgress.type === ActivityPipelineType.Flow ? 'flow' : 'regular',
-    eventId,
-    flowId: groupProgress.type === ActivityPipelineType.Flow ? entityId : null,
+    eventId: progressData.eventId,
+    flowId: groupProgress.type === ActivityPipelineType.Flow ? progressData.entityId : null,
   } as const;
 
   const navigateTo = activeAssessment.publicAppletKey
@@ -40,7 +89,7 @@ const NavigateToActiveAssessment = () => {
       })
     : ROUTES.survey.navigateTo({
         ...navigateParams,
-        targetSubjectId,
+        targetSubjectId: progressData.targetSubjectId,
       });
 
   return <Navigate to={navigateTo} />;
