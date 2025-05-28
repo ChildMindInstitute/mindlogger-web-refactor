2d810584bf9ded18d74c8c360b0199f9ed588b52
André Vital <andre@andrevital.com>
2025-05-28 00:26:41 -0600

    feat: Add RebrandBanner to Banner types

--- src/entities/banner/model/types.ts
+++ src/entities/banner/model/types.ts
@@ -1,6 +1,12 @@
 import { BannerProps } from '~/shared/ui/Banners/Banner';
 
-export const Banners = ['SuccessBanner', 'ErrorBanner', 'WarningBanner', 'InfoBanner'] as const;
+export const Banners = [
+  'SuccessBanner',
+  'ErrorBanner',
+  'WarningBanner',
+  'InfoBanner',
+  'RebrandBanner',
+] as const;
 export type BannerType = (typeof Banners)[number];
 
 export type BannerPayload = {
--- src/shared/ui/Banners/lib/const.ts
+++ src/shared/ui/Banners/lib/const.ts
@@ -5,6 +5,7 @@ import { BannerProps } from '../Banner';
 import { BannerType } from '~/entities/banner/model';
 import { ErrorBanner } from '~/shared/ui/Banners/ErrorBanner';
 import { InfoBanner } from '~/shared/ui/Banners/InfoBanner';
+import { RebrandBanner } from '~/shared/ui/Banners/RebrandBanner';
 import { SuccessBanner } from '~/shared/ui/Banners/SuccessBanner';
 import { WarningBanner } from '~/shared/ui/Banners/WarningBanner';
 
@@ -13,4 +14,5 @@ export const BannerComponents: Record<BannerType, ComponentType<BannerProps>> = 
   WarningBanner,
   ErrorBanner,
   InfoBanner,
+  RebrandBanner,
 };
