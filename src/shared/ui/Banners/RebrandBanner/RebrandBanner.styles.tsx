47c2f2b30073f7df3b446827fbbf25d9d7ec1d1c
André Vital <andre@andrevital.com>
2025-06-11 12:02:55 -0600

    feat: Add rebrand banner link element

--- src/shared/ui/Banners/RebrandBanner/RebrandBanner.styles.tsx
+++ src/shared/ui/Banners/RebrandBanner/RebrandBanner.styles.tsx
@@ -8,7 +8,4 @@ export const StyledImg = styled('img')({
 export const StyledLink = styled('a')({
   color: '#b6deff',
   textDecoration: 'underline',
-  '&:hover': {
-    textDecoration: 'none',
-  },
 });
