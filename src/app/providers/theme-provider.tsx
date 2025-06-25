6054e1c2780cfa0ae1d34b684c5d3166f79d598d
André Vital <andre@andrevital.com>
2025-06-24 22:26:17 -0600

    feat: Remove unused custom stylings

--- src/app/providers/theme-provider.tsx
+++ src/app/providers/theme-provider.tsx
@@ -212,37 +212,13 @@ const theme = createTheme({
         input: {
           fontFamily: variables.font.family.body,
         },
-        '.outline-none': {
-          outline: 'none !important',
-          boxShadow: 'none !important',
-        },
-        '.border-none': {
-          border: 'none !important',
-        },
-        '.disable-default-style': {
-          border: 'none',
-          outline: 'none',
-          background: 'none',
-          padding: 'none',
-          margin: 'none',
-        },
-        '.color-white': {
-          color: variables.palette.white,
-        },
-        '.color-white:hover': {
-          color: variables.palette.white,
-        },
-        '.hover': {
-          transition: '0.2s',
-        },
-        '.hover:hover': {
-          cursor: 'pointer',
-          boxShadow: '0 0px 2px 0 rgba(0, 0, 0, 0.1), 0 6px 15px 0 rgba(0, 0, 0, 0.16)',
-        },
-        'input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, input:-webkit-autofill:active':
-          {
-            boxShadow: 'inset 0 0 30px 30px rgba(255, 255, 255, 1)',
+        'input:-webkit-autofill': {
+          boxShadow: `inset 0 0 30px 30px ${variables.palette.white}`,
+
+          '&:hover, &:focus, &:active': {
+            boxShadow: `inset 0 0 30px 30px ${variables.palette.white}`,
           },
+        },
       }),
     },
     MuiAlert: {
