0cad070f4a4f1949e81d3fce828107be673c6674
Phillipe Bojorquez <phillipe.bojorquez@gmail.com>
2025-11-03 11:13:50 -0500

    Reset Password Tests

---
+++ tests/logged-out/reset-password.spec.ts
@@ -0,0 +1,63 @@
+import { test, expect } from '@playwright/test';
+import { login } from '../utils/loginPage.ts';
+
+test('User can reset their password when logged in', async ({ page }) => {
+    // Log in with valid credentials
+    await login(page, '/login', process.env.PLAYWRIGHT_EMAIL || '', process.env.PLAYWRIGHT_PASSWORD || '');
+    // Navigate to settings page
+    await page.getByRole('button', { name: 'Phillipe Bojorquez' }).click();
+    await page.getByRole('menuitem', { name: 'Settings' }).click();
+    // Verify navigation to settings page
+    await page.waitForURL('/protected/settings');
+    await expect(page.getByRole('heading', { name: 'Change Password' })).toBeVisible();
+    await expect(page.getByText(`Create the new password for ${process.env.PLAYWRIGHT_EMAIL}`)).toBeVisible();
+    
+    // Error page elements for empty password fields
+    const errorMessageOldPassword = page.getByText('Password must be at least 6').first();
+    const errorMessageNewPassword = page.getByText('Password must be at least 6').nth(1);
+    const errorMessageConfirmPassword = page.getByText('Password must be at least 6').nth(2);
+
+    // Submit empty password fields to trigger validation errors
+    await page.getByRole('button', { name: 'Submit' }).click();
+    // Assert that error messages are displayed
+    await expect(errorMessageOldPassword).toHaveText('Password must be at least 6 characters.');
+    await expect(errorMessageNewPassword).toBeVisible();
+    await expect(errorMessageConfirmPassword).toBeVisible();
+
+    // Verify passwords that do not match trigger appropriate error
+    await page.getByRole('textbox', { name: 'Old Password' }).fill('oldpassword123');
+    await page.getByRole('textbox', { name: 'New Password' }).fill('newpassword123');
+    await page.getByRole('textbox', { name: 'Confirm Password' }).fill('differentpassword123');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await expect(page.getByText('Passwords Do Not Match')).toBeVisible();
+
+    // Add password change steps that use the same old and new password
+    await page.getByRole('textbox', { name: 'Old Password' }).fill(process.env.PLAYWRIGHT_PASSWORD || '');
+    //todo new password 
+    await page.getByRole('textbox', { name: 'New Password' }).fill(process.env.PLAYWRIGHT_PASSWORD || '');
+    await page.getByRole('textbox', { name: 'Confirm Password' }).fill(process.env.PLAYWRIGHT_PASSWORD || '');
+    // Submit the password change form using the same old and new password
+    await page.getByRole('button', { name: 'Submit' }).click();
+    // Assert that system success message is displayed
+    await expect(page.getByTestId('system-success-message')).toHaveText('Password changed successfully.');
+
+    // Attempt to change password during reencryption process
+    // TODO you can get this state by changing password in the app and immediately trying to change it again
+    await page.getByRole('textbox', { name: 'Old Password' }).fill(process.env.PLAYWRIGHT_PASSWORD || '');
+    //todo new password 
+    await page.getByRole('textbox', { name: 'New Password' }).fill(process.env.PLAYWRIGHT_PASSWORD || '');
+    await page.getByRole('textbox', { name: 'Confirm Password' }).fill(process.env.PLAYWRIGHT_PASSWORD || '');
+    // Submit the password change form and verify using same password does not result in errors
+    await page.getByRole('button', { name: 'Submit' }).click();
+    // Assert that system error message is displayed
+    await expect(page.getByTestId('system-error-message')).toHaveText('Cannot change password. Reencryption process in progress.');
+});
+
+test('User can reset their password through the web interface', async ({ page }) => {
+    await page.goto('/login');
+    await page.click('text=Forgot Password?');
+    await page.fill('input[name="email"]', process.env.PLAYWRIGHT_EMAIL || '');
+    await page.click('button[type="submit"]');
+    const confirmationMessage = `Password reset link is sent to ${process.env.PLAYWRIGHT_EMAIL}`;
+    await expect(page.getByText(confirmationMessage)).toBeVisible();
+});
