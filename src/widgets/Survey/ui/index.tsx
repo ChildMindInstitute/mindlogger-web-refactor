0873f5c87c79417764e51a51fe5e8f8d896c065b
Chris Lei <chris.lei@childmind.org>
2026-01-29 07:20:56 -0800

    Use EnableFlowResume when fetching from server

--- src/widgets/Survey/ui/index.tsx
+++ src/widgets/Survey/ui/index.tsx
@@ -26,6 +26,9 @@ import {
   useModal,
   useOnceEffect,
 } from '~/shared/utils';
+import { isFlowResumeEnabled } from '~/shared/utils/featureFlags';
+import { useFeatureFlags } from '~/shared/utils/hooks';
+import { FeatureFlag } from '~/shared/utils/types/featureFlags';
 import { useEntitiesSync } from '~/widgets/ActivityGroups/model/hooks';
 
 type Props = {
@@ -101,13 +104,20 @@ export const SurveyWidget = (props: Props) => {
     error,
   } = useSurveyDataQuery({ publicAppletKey, appletId, activityId, targetSubjectId });
 
+  const { featureFlag } = useFeatureFlags();
+  const flowResumeFlag = featureFlag(FeatureFlag.EnableFlowResume, false);
+  const flowResumeEnabled = isFlowResumeEnabled(flowResumeFlag, appletId);
+
   const { data: completedEntities, isFetching } = useCompletedEntitiesQuery(
     {
       appletId,
       fromDate: formatToDtoDate(subMonths(new Date(), 1)),
       includeInProgress: true,
     },
-    { select: (data) => data.data.result, enabled: !publicAppletKey && !shouldRestart },
+    {
+      select: (data) => data.data.result,
+      enabled: flowResumeEnabled && !publicAppletKey && !shouldRestart,
+    },
   );
 
   // Sync with server
@@ -131,7 +141,7 @@ export const SurveyWidget = (props: Props) => {
     : null;
 
   useEffect(() => {
-    if (!currentActivityId || currentActivityId === activityId) return;
+    if (!flowResumeEnabled || !currentActivityId || currentActivityId === activityId) return;
 
     const navigateToProps = {
       appletId,
