ebcdb6fd08d20fe9ced900461b1951fd990883f6
Chris Lei <chris.lei@childmind.org>
2026-04-10 15:48:03 -0700

    Ditto for old password on change password form

--- src/features/ChangePassword/model/schema.ts
+++ src/features/ChangePassword/model/schema.ts
@@ -1,19 +1,12 @@
 import i18n from 'i18next';
 import { z } from 'zod';
 
-import {
-  ACCOUNT_PASSWORD_MIN_LENGTH,
-  ACCOUNT_PASSWORD_MIN_CHAR_TYPES,
-  LEGACY_PASSWORD_MIN_LENGTH,
-} from '~/shared/constants';
+import { ACCOUNT_PASSWORD_MIN_LENGTH, ACCOUNT_PASSWORD_MIN_CHAR_TYPES } from '~/shared/constants';
 import { checkPassword } from '~/shared/utils/passwordValidation';
 
 export const ChangePasswordSchema = () => {
   const { t } = i18n;
   const passwordRequired = t('validation.passwordRequired');
-  const oldPasswordMinLength = t('validation.passwordMinLength', {
-    chars: LEGACY_PASSWORD_MIN_LENGTH,
-  });
   const passwordMinLength = t('validation.passwordMinLength', {
     chars: ACCOUNT_PASSWORD_MIN_LENGTH,
   });
@@ -25,12 +18,7 @@ export const ChangePasswordSchema = () => {
 
   return z
     .object({
-      old: z
-        .string()
-        .trim()
-        .refine((value) => checkPassword(value, LEGACY_PASSWORD_MIN_LENGTH).meetsLength, {
-          message: oldPasswordMinLength,
-        }),
+      old: z.string().trim().min(1, { message: passwordRequired }),
       new: z
         .string()
         .trim()
