8e7dbde6c3edb4f4238a1f8c5edc5e211a648022
sricharan varanasi <59170910+sricharan-varanasi@users.noreply.github.com>
2026-06-11 15:18:30 -0400

    fix: use adaptive grid column for Save & Close button to prevent progress bar overlap

--- src/features/PassSurvey/ui/SurveyHeader/index.tsx
+++ src/features/PassSurvey/ui/SurveyHeader/index.tsx
@@ -55,7 +55,7 @@ const SurveyHeader = (props: Props) => {
         gridAutoFlow="column"
         alignItems="center"
         justifyContent="center"
-        gridTemplateColumns="1fr minmax(300px, 900px) 1fr"
+        gridTemplateColumns="minmax(0, 1fr) minmax(300px, 900px) minmax(max-content, 1fr)"
         gap={1.5}
       >
         {greaterThanSM && props.entityTimer && (
